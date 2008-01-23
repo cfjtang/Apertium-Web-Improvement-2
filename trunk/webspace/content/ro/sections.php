31ab095b23ded9977c7763d3449caa034c7b6c20
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-01-23 17:49:19 +0000

    nombre de sección...

--- trunk/webspace/content/ro/sections.php
+++ trunk/webspace/content/ro/sections.php
@@ -1,6 +1,6 @@
 <?php
 	$sections = array("whatisapertium", "whodevelopsit", "downloading", "testdrive", "documentation", "interact", "latestnews", "software", "funding", "users", "contact");
-	$text = array("ce este Apertium?", "cine elaborează Apertium?", "descărcări", "testați", "documentație", "interactionați!", "ultimele noutăți", "programe complementare", "finanțare", "utilizatori", "contact");
+	$text = array("ce este Apertium?", "cine dezvoltă Apertium?", "descărcări", "testați", "documentație", "interactionați!", "ultimele noutăți", "programe complementare", "finanțare", "utilizatori", "contact");
 
 $subsections = array
 (
