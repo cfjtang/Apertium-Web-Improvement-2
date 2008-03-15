b4869ea7942c7a47dfd7daa61a4fccebe3e9ccad
ebenimeli <ebenimeli@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-03-15 12:18:33 +0000

    minor add

---
+++ trunk/webspace/common/tt.php
@@ -0,0 +1,49 @@
+<?php
+	include_once("../config/apertium-config.php");
+
+	$text = $HTTP_GET_VARS["text"];
+	$dir = $HTTP_GET_VARS["dir"];
+	$mark = $HTTP_GET_VARS["mark"];
+
+	if($mark == 1) {
+		$markUnknown = "";
+	} else {
+		$markUnknown = "-u";
+	}
+	
+	$trad = translate($text, $dir, $markUnknown);
+	
+	print $trad;
+
+
+/*
+  **************************
+	   TRANSLATE
+	**************************
+*/
+function translate($text, $dir, $markUnknown) {
+	// Imports global vars (from config/apertium-config.php)
+	global $APERTIUM_TRANSLATOR;
+	global $LING_DATA_DIR;
+	
+	$text = stripslashes($text);
+	$tempfile = tempnam("tmp","tradtext");
+	
+  $fd = fopen($tempfile,"w");
+  fputs($fd, $text);
+  fclose($fd);
+  
+  if ($dir == "ro-es" ) {
+	$cmd = "cat $tempfile | LANG=en_GB.UTF-8 sed 's/ţ/ț/g' | LANG=en_GB.UTF-8 sed 's/ş/ș/g' | LANG=en_GB.UTF-8 $APERTIUM_TRANSLATOR -f txt $markUnknown $dir";
+  } else {
+  	$cmd = "LANG=es_ES.UTF-8 $APERTIUM_TRANSLATOR -f txt $markUnknown $dir $tempfile";
+  	}  
+  
+	$trad = shell_exec($cmd);
+	unlink($tempfile);
+	
+	return $trad;
+}
+
+
+?>
