d11a5c97f33f9e5308b401a32322971f8f868f50
jg18 <jg18@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-06-25 03:42:30 +0000

    adding apertium-ga-gd.ga.tsx tagger file

---
+++ trunk/apertium-ga-gd/apertium-ga-gd.ga.tsx
@@ -0,0 +1,65 @@
+<?xml version ="1.0" encoding="UTF-8"?>
+<tagger name = "Irish">
+  <tagset>
+    <def-label name="ADJ">
+      <tags-item tags="adj.*"/>
+    </def-label>
+    <def-label name="ADV">
+      <tags-item tags="adv.*"/>
+    </def-label>
+    <def-label name="CNJCOO" closed="true">
+      <tags-item tags="cnjcoo.*"/>
+    </def-label>
+    <def-label name="DET" closed="true">
+      <tags-item tags="det.*"/>
+    </def-label>
+    <def-label name="N_DAT">
+      <tags-item tags="n.dat.*"/>
+    </def-label>
+    <def-label name="N_F">
+      <tags-item tags="n.f.*"/>
+    </def-label>
+    <def-label name="N_M">
+      <tags-item tags="n.m.*"/>
+    </def-label>
+    <def-label name="N_PL">
+      <tags-item tags="n.pl.*"/>
+    </def-label>
+    <def-label name="N_SG">
+      <tags-item tags="n.sg.*"/>
+    </def-label>
+    <def-label name="NP">
+      <tags-item tags="np.*"/>
+    </def-label>
+    <def-label name="PR" closed="true">
+      <tags-item tags="pr.*"/>
+    </def-label>
+    <def-label name="VBLEX_CNI">
+      <tags-item tags="vblex.cni.*"/>
+    </def-label>
+    <def-label name="VBLEX_FTI">
+      <tags-item tags="vblex.fti.*"/>
+    </def-label>
+    <def-label name="VBLEX_GER">
+      <tags-item tags="vblex.ger.*"/>
+    </def-label>
+    <def-label name="VBLEX_IMP">
+      <tags-item tags="vblex.imp.*"/>
+    </def-label>
+    <def-label name="VBLEX_PAST">
+      <tags-item tags="vblex.past.*"/>
+    </def-label>
+    <def-label name="VBLEX_PII">
+      <tags-item tags="vblex.pii.*"/>
+    </def-label>
+    <def-label name="VBLEX_PP">
+      <tags-item tags="vblex.pp.*"/>
+    </def-label>
+    <def-label name="VBLEX_PRES">
+      <tags-item tags="vblex.pres.*"/>
+    </def-label>
+    <def-label name="VBLEX_PRS">
+      <tags-item tags="vblex.prs.*"/>
+    </def-label>
+  </tagset>
+</tagger>
