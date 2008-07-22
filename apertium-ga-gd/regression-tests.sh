62e60f49179ce8f446c6c90156975bb0dd9a30f3
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-07-22 17:30:33 +0000

    Add regression test script

---
+++ apertium-ga-gd/regression-tests.sh
@@ -0,0 +1,22 @@
+LIST=`wget -O - -q http://wiki.apertium.org/wiki/Welsh_to_English/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
+
+cp *.mode modes/
+
+for LINE in $LIST; do
+	dir=`echo $LINE | cut -f2 -d'(' | cut -f1 -d')'`;
+
+	if [ $dir = "en" ]; then
+		mode="en-cy";
+	elif [ $dir = "cy" ]; then
+		mode="cy-en";
+	else 
+		continue;
+	fi
+
+#	echo $LINE;
+	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
+	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g'`;
+
+	echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "`echo $SL | apertium -d . $mode`"\n";
+
+done
