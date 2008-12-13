1b7620a07b24c5bd4dda6cedfebb15e0ffd61e62
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-12-13 01:11:37 +0000

    foo

--- apertium-br-fr/regression-tests.sh
+++ apertium-br-fr/regression-tests.sh
@@ -15,14 +15,14 @@ for LINE in $LIST; do
 
 #	echo $LINE;
 	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
-	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g'`;
+	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g' | sed 's/^ //g' | sed 's/ $//g'`;
 	
 	TRANS=`echo $SL | apertium -d . $mode | sed 's/^ //g' | sed 's/ $//g'`;
 
-	if [[ $TL = $TRANS ]]; then
-		echo -e $mode"\t "$SL"\nWORKS\t$TL\n\t+ "$TRANS"\n";
+	if [[ $TL != $TRANS ]]; then
+		echo -e $mode"\t "$SL"\n\t- $TL\n\t+ "$TRANS"\n";
 	else
-		echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "$TRANS"\n";
+		echo -e $mode"\t "$SL"\nWORKS\t  $TL\n";
 	fi
 
 done
