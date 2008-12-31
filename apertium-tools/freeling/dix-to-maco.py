b5ec6a11aefaa56b11c4b02a51fbc8978123d4ed
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2008-12-31 15:43:28 +0000

    multiparaules

--- apertium-tools/freeling/dix-to-maco.py
+++ apertium-tools/freeling/dix-to-maco.py
@@ -27,8 +27,8 @@ sys.stdin  = codecs.getreader('utf-8')(sys.stdin);
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout);
 sys.stderr = codecs.getwriter('utf-8')(sys.stderr);
 
-if len(sys.argv) < 3: #{
-	print 'Usage: ./dix-to-maco.py <dix file> <parole lookup>';	
+if len(sys.argv) < 4: #{
+	print 'Usage: ./dix-to-maco.py [-l|-m|-n] <dix file> <parole lookup>';	
 	print '';
 	sys.exit(-1);
 #}
@@ -38,8 +38,9 @@ if commands.getstatusoutput('lt-expand')[0] > 256: #{
 	sys.exit(-1);
 #}
 
-infile = sys.argv[1];
-tffile = sys.argv[2];
+tipoxt = sys.argv[1];
+infile = sys.argv[2];
+tffile = sys.argv[3];
 
 transform = {};
 
@@ -63,25 +64,128 @@ for line in file(tffile).read().split('\n'): #{
 
 surface = {};
 
+mode = 0; # "General entries"
+
+if tipoxt == '-l': #{
+	mode = 1;
+#}
+
 for line in commands.getoutput('lt-expand ' + infile).split('\n'): #{
-	# We skip "generate only" entries
-	if line.count(':<:') > 0: #{
+	# We skip "generate only" entries and regexes
+	if line.count(':<:') > 0 or line.count('REGEXP') > 0: #{
 		continue;
 	#}
 
-	# We also skip inflecting multiwords (possibly deal with these later)
-	if line.count('#') > 0: #{
-		continue;
-	#}
-	
-	# And regexes get left aside too
-	if line.count('REGEXP') > 0: #{
-		continue;
-	#}
+	# Are we extracting locutions or main entries?
+	if mode == 1: #{
+		if line.count(' ') < 1 and line.count('#') < 1: #{
+			continue;
+		#}	
+	else:
+		if line.count(' ') > 0 or line.count('#') > 0: #{
+			continue;
+		#}
+	#}	
 
 	row = line.replace(':>:', ':').split(':');
 
-	if line.count('+') > 0: #{
+	case_flag = 0;
+	for c in row[0]: #{
+		if c.isupper(): #{
+			case_flag = 1;
+			continue;
+		#}
+	#}
+
+	# Skip proper names in the general entries
+	if tipoxt == '-n': #{
+		if case_flag == 0: #{
+			continue;
+		#}
+	else: #{
+		if case_flag == 1: #{
+			continue;
+		#}
+	#}
+
+
+
+	if line.count('#') > 0 and line.count(' ') < 0 and mode == 1: #{
+		#	bapurau newydd:>:papur<n><m><pl># newydd
+		#	cat bapurau newydd:>:cat papur<n><m><pl># newydd
+		# 	 -> <cat bapurau>
+		# we need to count the number of spaces backwards from the end
+		# of the right side in order to calculate where to place the '<>'
+		# as paradigms might throw out different number of spaces.
+			
+		# Skip these at the moment as we'll also need to work out how to 
+		# only put in one entry for the non-flecting part.
+		#
+
+		continue;
+
+	elif line.count('#') == 0 and line.count(' ') > 0 and mode == 1: #{
+		lemma = '';
+		analysis = '';
+		tags = '';
+		
+		if line.count('+') > 0: #{
+			superficial = row[0].strip().replace(' ', '_');
+			subreadings = row[1].strip().replace(' ', '_').split('+');
+
+			if superficial not in surface: #{
+				surface[superficial] = {};
+			#}
+
+			lemma = '';
+			analysis = '';
+			tags = '';
+
+			for reading in subreadings: #{
+				tag = '';
+				lemma_part = '';
+
+				idx = reading.index('<');
+				tag = reading[idx:];
+				lemma_part = reading.split('<')[0];
+
+				
+				if tag in transform: #{
+					tags = tags + transform[tag] + '+';
+				#}
+				lemma = lemma + lemma_part + '+';
+
+			#}
+
+			lemma = lemma.replace(' ', '_');
+			analysis = lemma.strip('+') + ' ' + tags.strip('+');
+			surface[superficial][analysis] = '';
+
+		else: #{	
+			superficial = row[0].strip().replace(' ', '_');
+			analysis = row[1].strip().replace(' ', '_');
+
+			if superficial not in surface: #{
+				surface[superficial] = {};
+			#}
+
+			tags = '';
+		
+			if '<' in analysis: #{
+				idx = analysis.index('<');
+				tags = analysis[idx:];
+				lemma = analysis.split('<')[0].replace(' ', '_');
+			#}
+		
+			if tags in transform: #{
+				analysis = lemma + ' ' + transform[tags];
+				surface[superficial][analysis] = '';
+			#}
+		#}
+	#}
+
+	if line.count('+') > 0 and mode == 0: #{
+
 		superficial = row[0].strip().replace(' ', '_');
 		subreadings = row[1].strip().split('+');
 
@@ -111,7 +215,8 @@ for line in commands.getoutput('lt-expand ' + infile).split('\n'): #{
 		lemma = lemma.replace(' ', '_');
 		analysis = lemma.strip('+') + ' ' + tags.strip('+');
 		surface[superficial][analysis] = '';
-	else: #{
+
+	elif mode == 0: #{
 
 		superficial = row[0].strip().replace(' ', '_');
 		analysis = row[1].strip();
