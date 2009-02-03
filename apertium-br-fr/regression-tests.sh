551442524a183fa2556f15d759c586f2fdf54249
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-02-03 18:18:20 +0000

    duals

--- apertium-br-fr/regression-tests.sh
+++ apertium-br-fr/regression-tests.sh
@@ -1,6 +1,7 @@
 LIST=`wget -O - -q http://wiki.apertium.org/wiki/Breton_and_French/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
 
-cp *.mode modes/
+#cp *.mode modes/
+basedir=/home/fran/svnroot/apertium/trunk/apertium-br-fr
 
 for LINE in $LIST; do
 	dir=`echo $LINE | cut -f2 -d'(' | cut -f1 -d')'`;
@@ -17,7 +18,7 @@ for LINE in $LIST; do
 	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
 	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g' | sed 's/^ *//g' | sed 's/ *$//g'`;
 	
-	TRANS=`echo $SL | apertium -d . $mode | sed 's/^ *//g' | sed 's/ *$//g'`;
+	TRANS=`echo $SL | apertium -d $basedir $mode | sed 's/^ *//g' | sed 's/ *$//g'`;
 
 	if [[ $TL != $TRANS ]]; then
 		echo -e $mode"\t "$SL"\n\t- $TL\n\t+ "$TRANS"\n";
