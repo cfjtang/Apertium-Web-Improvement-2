5a563e5e4bc4b422653f4e1c26b268b1f293cdab
sourcemorph <sourcemorph@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-03-31 13:01:34 +0000

    Added webservice plugin, local plugin checks for lang pairs, doesn't invoke sh now

--- trunk/apertium-tools/apertium-jubler-plugin/src/com/panayotis/jubler/tools/translate/AvailTranslators.java
+++ trunk/apertium-tools/apertium-jubler-plugin/src/com/panayotis/jubler/tools/translate/AvailTranslators.java
@@ -1,10 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.panayotis.jubler.tools.translate;
 
 import com.panayotis.jubler.tools.translate.plugins.ApertiumTranslator;
+import com.panayotis.jubler.tools.translate.plugins.ApertiumWebTranslator;
 import com.panayotis.jubler.tools.translate.plugins.GoogleHTMLTranslator;
 import com.panayotis.jubler.tools.translate.plugins.GoogleJSONTranslator;
 import java.util.Vector;
@@ -18,6 +15,7 @@ public class AvailTranslators extends Vector<Translator> {
     public AvailTranslators() {
         add(new GoogleHTMLTranslator());
         add(new ApertiumTranslator());
+        add(new ApertiumWebTranslator());
     //    add(new GoogleJSONTranslator());
     }
 
--- trunk/apertium-tools/apertium-jubler-plugin/src/com/panayotis/jubler/tools/translate/plugins/ApertiumTranslator.java
+++ trunk/apertium-tools/apertium-jubler-plugin/src/com/panayotis/jubler/tools/translate/plugins/ApertiumTranslator.java
@@ -1,122 +1,107 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.panayotis.jubler.tools.translate.plugins;
-import static com.panayotis.jubler.i18n.I18N._;
-import com.panayotis.jubler.subs.SubEntry;
+import com.panayotis.jubler.tools.translate.GenericApertiumTranslator;
 import com.panayotis.jubler.tools.translate.Language;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.io.OutputStream;
 import java.util.Vector;
+import static com.panayotis.jubler.i18n.I18N._;
 
 /**
  *
  * @author sourcemorph
  */
-public class ApertiumTranslator implements com.panayotis.jubler.tools.translate.Translator {
+public class ApertiumTranslator extends GenericApertiumTranslator {
+    private Vector<Language> sourceLang, destLang;
 
-    private static Vector<Language> lang;
-    static {
-        lang = new Vector<Language>();
-        lang = new Vector<Language>();
-        lang.add(new Language("ar", _("Arabic")));
-        lang.add(new Language("bg", _("Bulgarian")));
-        lang.add(new Language("zh-CN", _("Chinese")));
-        lang.add(new Language("hr", _("Croatian")));
-        lang.add(new Language("cs", _("Czech")));
-        lang.add(new Language("da", _("Danish")));
-        lang.add(new Language("nl", _("Dutch")));
-        lang.add(new Language("en", _("English")));
-        lang.add(new Language("fi", _("Finnish")));
-        lang.add(new Language("fr", _("French")));
-        lang.add(new Language("de", _("German")));
-        lang.add(new Language("el", _("Greek")));
-        lang.add(new Language("hi", _("Hindi")));
-        lang.add(new Language("it", _("Italian")));
-        lang.add(new Language("ja", _("Japanese")));
-        lang.add(new Language("ko", _("Korean")));
-        lang.add(new Language("no", _("Norwegian")));
-        lang.add(new Language("pl", _("Polish")));
-        lang.add(new Language("pt", _("Portuguese")));
-        lang.add(new Language("ro", _("Romanian")));
-        lang.add(new Language("ru", _("Russian")));
-        lang.add(new Language("es", _("Spanish")));
-        lang.add(new Language("sv", _("Swedish")));
-    }
-
-    public String getDefaultSourceLanguage() {
-        return _("English");
-    }
-
-    public String getDefaultDestinationLanguage() {
-        return _("Spanish");
-    }
-
-    protected Vector<Language> getLanguages() {
-        return lang;
-    }
-
-    public String[] getSourceLanguages() {
-        String[] langs = new String[lang.size()];
-        for (int i = 0; i < lang.size(); i++) {
-            langs[i] = lang.get(i).getName();
+    public ApertiumTranslator() {
+        sourceLang = new Vector<Language>();
+        destLang = new Vector<Language>();
+        try {
+            Process Findspace = Runtime.getRuntime().exec("apertium xxx");
+            BufferedReader Resultset = new BufferedReader(new InputStreamReader(Findspace.getInputStream()));
+            String current = Resultset.readLine();
+            //System.out.println(current);
+            current = Resultset.readLine();
+            do {
+                //System.out.println(current);
+                current = current.substring(3);
+                String t1,t2;
+                t1 = current.substring(0,2);
+                t2 = current.substring(3);
+                String fullt1 = findLanguageID(t1);
+                String fullt2 = findLanguageID(t2);
+                sourceLang.add(new Language(t1, _(fullt1)));
+                destLang.add(new Language(t2, _(fullt2)));
+                //System.out.print(t1+"--"+fullt1);
+                //System.out.println(t2+"--"+fullt2);
+                current = Resultset.readLine();
+            } while(current!=null);
+        } catch(IOException ex) {
+            ex.printStackTrace();
         }
-        return langs;
-    }
-
-    public String[] getDestinationLanguagesFor(String from) {
-        String[] langs = new String[lang.size()];
-        for (int i = 0; i < lang.size(); i++) {
-            langs[i] = lang.get(i).getName();
-        }
-        return langs;
     }
 
     public String getDefinition() {
         return _("Apertium translate");
     }
 
-    public String findLanguage(String language) {
-        for (Language l : getLanguages()) {
-            if (l.getName().equals(language)) {
-                return l.getID();
-            }
-        }
-        return "";
+    public String getDefaultSourceLanguage() {
+        System.out.println("default s.language");
+        return _(sourceLang.get(0).getName());
     }
 
-    public boolean translate (Vector<SubEntry> subs, String from_language, String to_language) {
-        Iterator<SubEntry> it = subs.iterator();
-        SubEntry current;
-        String transSent, so_lang, de_lang;
-        //System.out.println(from_language);
-        //System.out.println(to_language);
-        so_lang = findLanguage(from_language);
-        de_lang = findLanguage(to_language);
-        while(it.hasNext()) {
-            current = it.next();
-            transSent = current.getText();
-            String[] cmd = {
-            "/bin/sh",
-            "-c",
-            "echo \"" + current.getText() + "\" | apertium " + so_lang + "-" + de_lang
-            };
-            System.out.println(cmd);
+    public String getDefaultDestinationLanguage() {
+        System.out.println("default d.language");
+        return _(destLang.get(0).getName());
+    }
+
+    public String[] getSourceLanguages() {
+        System.out.println("Source Lang=");
+        String[] langs = new String[sourceLang.size()];
+        for (int i = 0; i < sourceLang.size(); i++) {
+            langs[i] = sourceLang.get(i).getName();
+            System.out.println(langs[i]);
+        }
+        return langs;
+    }
+
+    public String[] getDestinationLanguagesFor(String from) {
+        System.out.println("Dest Lang=");
+        Vector<String> temp = new Vector<String>();
+        String [] langs;
+        for(int i=0; i<sourceLang.size(); i++) {
+            Language l = sourceLang.get(i);
+            if(l.getName().equals(from)) {
+                temp.add(destLang.get(i).getName());
+            }
+        }
+        langs = new String[temp.size()];
+        int count = 0;
+        for(String s : temp) {
+            langs[count++] = s;
+            System.out.println(langs[count]);
+        }
+        return langs;
+    }
+
+    public String machineTranslation (String text, String so_lang, String de_lang) {
+        String result = "";
+        String[] cmd = {"apertium",  so_lang + "-" + de_lang};
             try {
                 Process Findspace = Runtime.getRuntime().exec(cmd);
+                OutputStream output = Findspace.getOutputStream();
+                output.write(text.getBytes());
+                output.close();
                 BufferedReader Resultset = new BufferedReader(new InputStreamReader (Findspace.getInputStream(), "UTF-8"));
-                transSent = Resultset.readLine();
-                System.out.println(transSent);
-            }catch(IOException ex) {
+                result = Resultset.readLine();
+                Resultset.close();
+                Findspace.destroy();
+            } catch(IOException ex) {
                 System.out.println("Could not translate, perhaps Apertium isn't installed correctly");
                 ex.printStackTrace();
             }
-            current.setText(transSent);
-        }
-        return true;
+        return result;
     }
 }
