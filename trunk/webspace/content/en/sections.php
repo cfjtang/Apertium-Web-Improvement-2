d8e6efa2f636914976b3893de811f481086e33e5
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-05-18 13:51:30 +0000

    commit for mikel

--- trunk/webspace/content/en/sections.php
+++ trunk/webspace/content/en/sections.php
@@ -1,47 +1,7 @@
 <?php
-	//$sections = array("whatisapertium", "testdrive", "documentation", "downloading", "contact");
 	$sections = array("translatetext", "whatisapertium", "downloading", "documentation", "contact");
-	$text = array("about", "demo", "documentation", "download",  "contact");
 	$text = array("demo", "about", "download", "documentation",  "contact");
 
-$subsections = array
-(
-/*
-"whatisapertium" =>array
-   (
-   "langpairs"=>"Language pairs",
-   ),
-   */
-/*   "testdrive"=>array
-   (
-   	"translatetext"=>"Text translation",
-   	"translatedoc"=>"Document translation",
-   	"surfandtranslate"=>"Surf &amp; translate",
-	"lookup"=>"DicLookUp",   	
-   	"link@Apertium unstable"=>"http://xixona.dlsi.ua.es/apertium-unstable/", 
-   	"link@Apertium alpha-testing"=>"http://xixona.dlsi.ua.es/testing/"   	
-   ), 
-"documentation" => array
-   (
-   "documents"=>"Documents",
-   "link@publications"=>"http://wiki.apertium.org/wiki/Publications",
-   "link@Wiki" => "http://wiki.apertium.org/"
-   ),*/
-"interact" =>array
-	(
-		"link@cgi::irc"=>"http://xixona.dlsi.ua.es/cgi-bin/cgiirc/irc.cgi",
-/*		"link@Forum"=>"http://sourceforge.net/forum/forum.php?forum_id=481030", */
-		"link@Mailing list"=>"https://lists.sourceforge.net/lists/listinfo/apertium-stuff"
-	) /*,
-"software" =>array
-	(
-		"apertium-tagger-training-tools"=>"Tagger training tools",
-		"apertium-transfer-tools"=>"Transfer tools",
-		"lttoolbox"=>"Lexical transformation",
-		"apertium-eval-translator"=>"Evaluation",
-		"apertium-tinylex"=>"TinyLex"
-	), */
-);
-
+	$subsections = array();
 ?>
 
--- trunk/webspace/content/ro/sections.php
+++ trunk/webspace/content/ro/sections.php
@@ -1,47 +1,7 @@
 <?php
-	$sections = array("whatisapertium", "whodevelopsit", "downloading", "testdrive", "documentation", "interact", "latestnews", "software", "funding", "users", "contact");
-	$text = array("ce este Apertium?", "cine dezvoltă Apertium?", "descărcări", "testați", "documentație", "interactionați!", "ultimele noutăți", "programe complementare", "finanțare", "utilizatori", "contact");
+	$sections = array("translatetext", "whatisapertium", "downloading", "documentation", "contact");
+	$text = array("testați", "ce este Apertium?", "descărcări", "documentație", "contact");
 
-$subsections = array
-(
-/*
-"whatisapertium" =>array
-   (
-   "langpairs"=>"Language pairs",
-   ),
-   */
-   "testdrive"=>array
-   (
-   	"translatetext"=>"Traducere de texte",
-   	"translatedoc"=>"Traducere de documente",
-   	"surfandtranslate"=>"Navigare și traducere",
-		"lookup"=>"DicLookUp"   	
-   ),
-"documentation" =>array
-   (
-   /*"documents"=>"Documents",*/
-   "publications"=>"Publications",
-   "link@Wiki" => "http://xixona.dlsi.ua.es/wiki"
-   ),
-"interact" =>array
-	(
-		"link@cgi::irc"=>"http://xixona.dlsi.ua.es/cgi-bin/cgiirc/irc.cgi",
-		"link@Forum"=>"http://sourceforge.net/forum/forum.php?forum_id=481030",
-		"link@Mailing list"=>"https://lists.sourceforge.net/lists/listinfo/apertium-stuff"
-	),
-"latestnews" =>array
-	(
-		"link@RSS"=>"http://www.sf.net/export/rss2_projnews.php?group_id=143781"
-	),
-"software" =>array
-	(
-		"apertium-tagger-training-tools"=>"Tagger training tools",
-		"apertium-transfer-tools"=>"Transfer tools",
-		"lttoolbox"=>"Lexical transformation",
-		"apertium-eval-translator"=>"Evaluation",
-    		"apertium-tinylex"=>"TinyLex"
-	),
-);
-
+	$subsections = array();
 ?>
 
