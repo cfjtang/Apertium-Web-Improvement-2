8dbee57e4f4e38e930f0c4ac2dfa4be43d6bd51d
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-05-25 17:29:10 +0000

    fix two

--- apertium-ga-gd/dev/inconsistency.sh
+++ apertium-ga-gd/dev/inconsistency.sh
@@ -1,6 +1,6 @@
 TMPDIR=/tmp
-lt-expand ../apertium-sv-da.sv.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-ga-gd.ga.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../apertium-sv-da.sv-da.t1x  ../sv-da.t1x.bin  ../sv-da.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../sv-da.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-ga-gd.ga-gd.t1x  ../ga-gd.t1x.bin  ../ga-gd.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../ga-gd.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
