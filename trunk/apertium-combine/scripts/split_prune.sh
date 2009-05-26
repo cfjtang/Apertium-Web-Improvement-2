90a7099524773c1a80bb17c6e45bbe473ab81423
snippyhollow <snippyhollow@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-05-26 16:34:12 +0000

    last versions of replace.p and split_prune.sh

--- trunk/apertium-combine/scripts/split_prune.sh
+++ trunk/apertium-combine/scripts/split_prune.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Usage "sh split_prune.sh phrase-table" with the good div and exe
-if [$1 -eq '']
+div=30000
+exe=/Users/snippy/apertium/svn/apertium/trunk/apertium-combine/pruning/a.out
+
+if [ "$1" ]
 then
+    echo ">>> Spliting phrase table in chunks"
+else 
     echo "Usage: sh split_prune.sh phrase-table"
     exit
 fi
 
-div=30000
-exe=/Users/snippy/apertium/svn/apertium/trunk/apertium-combine/pruning/fpruning.py
 #amount=`wc -l $1`
 #amount=`echo $amount | sed 's/[^0-9]*//g'`
 split -l $div $1 chunk
@@ -16,9 +19,14 @@ split -l $div $1 chunk
 #echo ">>>" $amount "lines has been divided in" $chunks "chunks"
 count=`ls | grep chunk | wc -w`
 echo ">>>" $count "chunks created"
+table=`ls | grep chunk`
+echo $table
+echo `ls | grep chunk`
+
 for f in `ls | grep chunk`
 do
-    $exe -i $f -o _$f
+    echo ">>> launched $exe on $f"
+    $exe $f _$f
 done
 cat _chunk* > pruned_phrase-table
 
