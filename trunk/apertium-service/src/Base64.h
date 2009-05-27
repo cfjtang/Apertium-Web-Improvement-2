daa79d71579dff07afea92ecd8b1982a56e3167e
pminervini <pminervini@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-05-27 18:21:39 +0000

    fixes

--- trunk/apertium-service/src/Base64.h
+++ trunk/apertium-service/src/Base64.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2009  Pasquale Minervini <p.minervini@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BASE64_H_
 #define BASE64_H_
 
@@ -100,6 +117,5 @@ private:
 };
 
 const std::string Base64::base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
 
 #endif /* BASE64_H_ */
