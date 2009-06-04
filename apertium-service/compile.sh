eaf33d64f8524a437415bfed3762d084a10b7c82
pminervini <pminervini@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-06-04 14:45:34 +0000

    + -ltextcat

--- apertium-service/compile.sh
+++ apertium-service/compile.sh
@@ -3,4 +3,5 @@ g++ -L/usr/local/lib -o ApertiumServer src/*.cpp src/core/*.cpp -I/usr/include/l
 -I/usr/include/libxml2 -I/usr/include/glibmm-2.4 -I/usr/lib/glibmm-2.4/include -I/usr/include/sigc++-2.0 \
 -I/usr/lib/sigc++-2.0/include -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/local/include/ -liqxmlrpc \
 -llttoolbox3 -lapertium3 -lboost_regex-mt -lglib -lboost_program_options-mt -lboost_filesystem-mt -lxml2 \
--lgobject-2.0 -lboost_thread-mt -lsigc-2.0 -lglib-2.0 -lxml++-2.6 -liqxmlrpc-server -Isrc/ -Isrc/core/ -Isrc/format/
+-lgobject-2.0 -lboost_thread-mt -lsigc-2.0 -lglib-2.0 -lxml++-2.6 -liqxmlrpc-server -Isrc/ -Isrc/core/ -Isrc/format/ \
+-ltextcat
