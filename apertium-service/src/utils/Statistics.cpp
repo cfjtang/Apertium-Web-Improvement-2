838b228790e67fad51a1b68a51a49f0b436eae78
pminervini <pminervini@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-06-30 13:29:38 +0000

    Halley's commit - you only see this every 75 years

---
+++ apertium-service/src/utils/Statistics.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009  Pasquale Minervini <p.minervini@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Statistics.h"
+
+#include <iostream>
+#include <string>
+
+Statistics *Statistics::instance = NULL;
+boost::mutex Statistics::instanceMutex;
+
+Statistics *Statistics::Instance() {
+	boost::mutex::scoped_lock Lock(instanceMutex);
+	if (!instance)
+		instance = new Logger();
+	return (instance);
+}
+
+Logger::Logger() { }
+
+Logger::~Logger() {
+	boost::mutex::scoped_lock Lock(instanceMutex);
+	if (instance != NULL) {
+		instance = NULL;
+	}
+}
