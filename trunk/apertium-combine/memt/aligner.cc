73f406996a4d36cbd3cb6e2d197078635c73ceb2
snippyhollow <snippyhollow@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-03 17:13:24 +0000

    Most basic aligner, can be improved by:  * precomputing the matching words (replaces n! comparisons by n^2 comparisons and n! lookups in the worst case) [1]  * different word matchers (for instance: we could take synonyms into account)  * different align() heuristic (currently: __maximal consecutive alignments__, could be maximal crossing edges)  * align punctuation
    This two last things should be implemented with external objects (strategy pattern) to be able to instantiate them at runtime as an option.
    
    Currently makes the longuest alignment line by line from two (w)fstreams.
    The result is in std::vector<int> _final_alignment with 
    _final_alignment[indice _word_right] = indice of the mapped _word_left 
    when this indice is -1, it means that this words are unaligned, this gives freedom for the hypothesis generator.
    
    /!\ this doesn't align all that can be aligned, see examples. Will make more alignments in a next version (with [1]).
    
    To try:
    cd apertium-combine/memt/
    make
    ./aligner ../test/apertium.in ../test/moses.in

--- trunk/apertium-combine/memt/aligner.cc
+++ trunk/apertium-combine/memt/aligner.cc
@@ -9,18 +9,24 @@ int main(int argc, char** argv)
 { 
     char* input_file1 = argv[1];
     char* input_file2 = argv[2];
-    wfstream file_in1(input_file1); //, ios::in);
-    wfstream file_in2(input_file2); //, ios::in);
+    wfstream file_in1(input_file1); 
+    wfstream file_in2(input_file2); 
     wstring input_line1;
     wstring input_line2;
     cout << "Reading from " << input_file1 << " and " << input_file2 << endl;
+    // ugly hack to avoid eating the ending line
+    getline(file_in1, input_line1);
+    getline(file_in2, input_line2);
     while(file_in1 && file_in2) {
-        getline(file_in1, input_line1);
-        getline(file_in2, input_line2);
+        if (!input_line1.empty() && !input_line2.empty()) {
+            wcout <<"lala"<<endl;
+        }
+        wcout << "Align: " << input_line1 << " <with> " << input_line2 << endl;
         Alignment a = Alignment(input_line1, input_line2);
         a.align();
         a.print();
-        //cout << a.print_result() << endl;
+        getline(file_in1, input_line1);
+        getline(file_in2, input_line2);
     }
     return 0;
 }
