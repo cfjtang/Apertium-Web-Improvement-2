dd16ddfde1e1400a7e451debccb99f6f8632220c
jimregan <jimregan@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-03 11:30:32 +0000

    intltool-extract --type=gettext/glade verbconj.glade

--- apertium-verbconj/po/verbconj.glade.h
+++ apertium-verbconj/po/verbconj.glade.h
@@ -1,19 +1,19 @@
-char *s = _("About");
-char *s = _("Conjugate");
-char *s = _("Copyright &#xA9; 2009 Brendan Molloy");
-char *s = _("Custom filter: ");
-char *s = _("Displays all possible verb inflections for a given Apertium dictionary.");
-char *s = _("Information");
-char *s = _("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n"
+char *s = N_("About");
+char *s = N_("Conjugate");
+char *s = N_("Copyright &#xA9; 2009 Brendan Molloy");
+char *s = N_("Custom filter: ");
+char *s = N_("Displays all possible verb inflections for a given Apertium dictionary.");
+char *s = N_("F_ilters");
+char *s = N_("Information");
+char *s = N_("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n"
              "\n"
              "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. \n"
              "\n"
              "You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.");
-char *s = _("_Default");
-char *s = _("_Edit");
-char *s = _("_Export");
-char *s = _("_File");
-char *s = _("F_ilters");
-char *s = _("_Help");
-char *s = _("_Preferences");
-char *s = _("button");
+char *s = N_("_Default");
+char *s = N_("_Edit");
+char *s = N_("_Export");
+char *s = N_("_File");
+char *s = N_("_Help");
+char *s = N_("_Preferences");
+char *s = N_("button");
