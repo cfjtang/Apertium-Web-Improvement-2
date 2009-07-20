bbb4e677e4daa5bf108588528d94f4cc327cceca
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-20 20:21:14 +0000

    fix some things

---
+++ apertium-combine/memt/moses_collector.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cwchar>
+#include <string>
+
+#include "moses_collector.hh"
+
+using namespace std;
+
+
+MosesCollector::MosesCollector()
+{
+
+}
+
+MosesCollector::~MosesCollector()
+{
+
+}
+
+wchar_t*
+MosesCollector::translate(wchar_t *block)
+{
+	wchar_t *translation = L"";
+
+	return translation;
+
+}
