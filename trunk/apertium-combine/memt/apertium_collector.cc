d4a51aea77e2e969b7ea2c7fbee4e7a7bf6e7093
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-20 19:42:17 +0000

    add more files

---
+++ trunk/apertium-combine/memt/apertium_collector.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cwchar>
+#include <string>
+
+#include "collector.hh"
+
+using namespace std;
+
+int 
+main(int argc, char **argv) 
+{
+	FILE *input = stdin;
+	FILE *output = stdout;		
+	Collector c;
+
+	if(setlocale(LC_CTYPE, "") == NULL) {
+		wcerr << L"Warning: unsupported locale, fallback to \"C\"" << endl;
+		setlocale(LC_ALL, "C");
+	}
+
+	c.collect(output, input);
+
+	return 0;
+}
