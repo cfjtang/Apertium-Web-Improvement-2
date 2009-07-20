d3244ccd3eb09d7c71b3cf36bf010ccf05cb292b
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-20 19:44:43 +0000

    add header oops

---
+++ apertium-combine/memt/collector.hh
@@ -0,0 +1,19 @@
+#ifndef __COLLECTOR_H__
+#define __COLLECTOR_H__
+
+#include <iostream>
+#include <cwchar>
+#include <string>
+
+using namespace std;
+
+class Collector {
+
+public:
+	Collector();
+	~Collector();
+
+	void collect(FILE *input, FILE *output);
+};
+
+#endif /* __COLLECTOR_H__ */
