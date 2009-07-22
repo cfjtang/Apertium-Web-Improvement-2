0d3f135cbb0ee7cf759c73363caee4c5a0630b43
snippyhollow <snippyhollow@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-22 21:20:52 +0000

    memt: operator< is > for Hypothesis

--- trunk/apertium-combine/memt/ranker.hh
+++ trunk/apertium-combine/memt/ranker.hh
@@ -17,6 +17,25 @@ struct Hypothesis;
 
 using namespace std;
 
+//bool ranker_bin_pred(const Hypothesis& h1, const Hypothesis& h2) 
+//{
+//    return h1.score > h2.score;
+//}
+
+
+    // Sorting using a binary predicate:
+    // // An alternative (using the container container pointers instead
+    // // of values) that doesn't need a binary predicate is to define
+    // // your own operator<() which sort() uses by default.
+    // bool BinPred( const Class& o1, const Class& o2 )
+    // {
+    //     return o1.val < o2.val;
+    //     }
+    //     vector vec;
+    //     sort( vec.begin(), vec.end(), BinPred );
+    //
+    //
+
 class Ranker
 {
     public:
