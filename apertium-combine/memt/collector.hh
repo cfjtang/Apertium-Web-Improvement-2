1fef590c08af6e27767dc1196075a62804ebfc89
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-22 22:27:05 +0000

    some licence stuff

--- apertium-combine/memt/collector.hh
+++ apertium-combine/memt/collector.hh
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2009 Apertium project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
 #ifndef __COLLECTOR_H__
 #define __COLLECTOR_H__
 
@@ -14,7 +33,7 @@ public:
 	~Collector();
 
 	void collect(FILE *input, FILE *output);
-	wchar_t* translate(const wchar_t* block);
+	wstring* translate(wstring *block);
 };
 
 #endif /* __COLLECTOR_H__ */
