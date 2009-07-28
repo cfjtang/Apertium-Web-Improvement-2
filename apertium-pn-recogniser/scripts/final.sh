814dde546b8ebb75e640d06a0a36821ab0424c5b
japerez <japerez@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-07-28 12:42:13 +0000

    Double sed added to final.sh for the cases unknown words marked/unmarked.

--- apertium-pn-recogniser/scripts/final.sh
+++ apertium-pn-recogniser/scripts/final.sh
@@ -1,3 +1,6 @@
 
-# Elimina *_Added_by_Proper_Noun_Detector_
-sed -r 's/\*_Added_by_Proper_Noun_Detector_//g'
+# Removes *_Added_by_Proper_Noun_Detector_ or _Added_by_Proper_Noun_Detector_ (in case disabling the
+# marking of unknown words has been selected)
+sed -r 's/\*_Added_by_Proper_Noun_Detector_//g' |
+sed -r 's/_Added_by_Proper_Noun_Detector_//g'
+
