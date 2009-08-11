2c40698696378a097c991648eebedf0050b7263c
nordfalk <nordfalk@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-08-11 09:12:31 +0000

    fix n

--- trunk/apertium-sv-da/dev/lt-expand-grep.sh
+++ trunk/apertium-sv-da/dev/lt-expand-grep.sh
@@ -1,2 +1,4 @@
 read word
-lt-expand $1 | grep "$word" | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g'
+word=`echo $word|cut -d. -f1`
+#echo $word
+lt-expand $1 | grep "$word<" | sed 's/:<:/%/g' | sed 's/:/ ^/g' | sed 's/$/$ ^.<sent>$/g'
