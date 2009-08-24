f10dcae0d96d8ff14ce768b370789393e609747d
snippyhollow <snippyhollow@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-08-24 08:54:53 +0000

    memt: sources for the parallel scan generator (not working)

---
+++ trunk/apertium-combine/memt/parallel_scan_generator.cc
@@ -0,0 +1,10 @@
+#include "parallel_scan_generator.hh"
+
+using namespace std;
+
+void Parallel_Scan_Generator::generate(Alignment& a, std::list<Hypothesis>& h)
+{
+    for (unsigned int mt = 0; mt < a._mt_translations.size(); ++mt) {
+        ;
+    }
+}
