a2b90390307245f3055b83717be384210e2f8484
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-09-12 11:21:13 +0000

    fix

--- apertium-service/examples/test.py
+++ apertium-service/examples/test.py
@@ -7,7 +7,20 @@ import xmlrpclib, sys;
 #proxy = xmlrpclib.ServerProxy("http://localhost:6173/RPC2");
 proxy = xmlrpclib.ServerProxy("http://localhost:8080/RPC2");
 
+sys.stdout.write('* Enabled pairs: ');
+count = 0;
 for pair in proxy.languagePairs(): #{
 	sys.stdout.write(pair["srcLang"] + "-" + pair["destLang"] + " ");
+	count = count + 1;
+
+	if count == 10: #{
+		count = 0;
+		print '';
+		sys.stdout.write('    ');
+	#}
 #}
 print "";
+
+res = proxy.translate('<i>una prova</i>', 'ca', 'en', 'html');
+
+print '* <i>una prova</i> → ' + res['translation'];
