fc4beb1ad915bb14a90a794a0eeb731b0e4d9846
ftyers <ftyers@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-09-15 11:59:00 +0000

    add preliminary files

---
+++ trunk/apertium-combine/memt/case_insensitive_morph_matcher.cc
@@ -0,0 +1,69 @@
+#include "case_insensitive_morph_matcher.hh"
+#include <iostream>
+#include <cctype>
+#include <utility>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+void inline to_lower(wstring& to_lower)
+{
+    transform(to_lower.begin(), to_lower.end(),
+      to_lower.begin(), towlower);
+    return;
+}
+
+int Case_Insensitive_Morph_Matcher::match(const wstring& left, const wstring& right)
+{
+    wstring l = wstring(left);
+    wstring r = wstring(right);
+    to_lower(l);
+    to_lower(r);
+    if (!l.compare(r))
+        return 0;
+
+
+    readBil("/home/fran/Source/apertium/trunk/apertium-cy-en/cy-en.automorf.bin");                                 
+
+    pair<wstring, int> tr;
+    tr = fstp.biltransWithQueue(*tmpword[0], false);
+
+    
+
+    wstring sl;
+    if (l.size() > 5) {
+        sl = l.substr(0, l.size() - 3);
+        if (!sl.compare(r)) {
+            wcout << sl << " ** " << r << endl;
+            return 0;
+        }
+    }
+    if (r.size() > 5) {
+        wstring sr = r.substr(0, r.size() - 3);
+        if (!sr.compare(l)) {
+            wcout << sr << " ** " << l << endl;
+            return 0;
+        }
+        if (l.size() > 5)
+            if (!sr.compare(sl))
+                return 0;
+    }
+    return 42;
+}
+
+void
+Case_Insensitive_Morph_Matcher::readBil(string const &fstfile)
+{
+  FILE *in = fopen(fstfile.c_str(), "rb");
+  if(!in)
+  {
+    cerr << "Error: Could not open file '" << fstfile << "'." << endl;
+    exit(EXIT_FAILURE);
+  }
+  fstp.load(in);
+  fstp.initBiltrans();
+  fclose(in);
+}
+
