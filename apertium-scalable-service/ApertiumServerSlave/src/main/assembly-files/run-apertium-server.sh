9d95dc9bcf534d9cf07be5e85387f18444fc1bb1
vitaka <vitaka@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-12-02 20:41:46 +0000

    locale bug fix

--- apertium-scalable-service/ApertiumServerSlave/src/main/assembly-files/run-apertium-server.sh
+++ apertium-scalable-service/ApertiumServerSlave/src/main/assembly-files/run-apertium-server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
  
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
-export LANG=en_US.UTF-8
+
+export LANG="es_ES.utf8"
+export LC_ALL="es_ES.utf8"
+
 
 echo $$ >.pid
 
