fb042626112bd185010dfba344361cf4d7a6463b
pminervini <pminervini@72bbbca6-d526-0410-a7d9-f06f51895060>
2009-12-13 14:25:17 +0000

    small update

--- apertium-combine/memt/case_insensitive_morph_matcher.cc
+++ apertium-combine/memt/case_insensitive_morph_matcher.cc
@@ -90,6 +90,6 @@ void Case_Insensitive_Morph_Matcher::readBil(string const &fstfile)
   fstp.initBiltrans();
   fclose(in);
 
-  cerr << "Loaded transducer from " << fstfile << endl;
+  //cerr << "Loaded transducer from " << fstfile << endl;
 }
 
