7860309c2ae018aa14b95ff1cbfcecf0a09e6118
vitaka <vitaka@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-02-24 09:00:21 +0000

    init.d scripts

--- scaleMT/ScaleMTSlave/src/main/assembly-files/run-scaleMT.sh
+++ scaleMT/ScaleMTSlave/src/main/assembly-files/run-scaleMT.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
  
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
-export LANG=en_US.UTF-8
+export LANG="es_ES.utf8"
+export LC_ALL="es_ES.utf8"
 
 echo $$ >.pid
 
-exec java -Djava.rmi.server.hostname="$1"  -jar ScaleMTSlave-1.0.jar -host $1 -RMIname ScaleMTSlave -RMIport 1331 $2
+exec java -Djava.rmi.server.hostname="$1"  -jar `dirname $0`/ScaleMTSlave-1.0.jar -host $1 -RMIname ScaleMTSlave -RMIport 1331 $2
