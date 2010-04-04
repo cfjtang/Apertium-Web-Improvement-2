847b76d128ae906eaece364a2fb78650c9b982ec
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-04-04 13:27:33 +0000

    some fixes

--- apertium-br-fr/dev/inconsistency.sh
+++ apertium-br-fr/dev/inconsistency.sh
@@ -1,7 +1,8 @@
 TMPDIR=/tmp
 
-lt-expand ../apertium-br-fr.br.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
-	cg-proc ../br-fr.rlx.bin|
+lt-expand ../apertium-br-fr.br.dix | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.\/.<sent>$/g' | sed 's/^\^/^foo\//g' | tee $TMPDIR/tmp_testvoc1.txt |
+	cg-proc ../br-fr.rlx.bin |
+        apertium-tagger -g ../br-fr.prob  |
         apertium-pretransfer|
         apertium-transfer ../apertium-br-fr.br-fr.t1x  ../br-fr.t1x.bin  ../br-fr.autobil.bin |
         apertium-interchunk ../apertium-br-fr.br-fr.t2x  ../br-fr.t2x.bin |
