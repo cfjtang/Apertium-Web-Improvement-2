0d0708b898c6cb9fb991fa9e4c9c2b848478d74f
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-04-18 11:23:10 +0000

    move

--- apertium-tools/geriaoueg_new/newg/index.php
+++
@@ -1,9 +0,0 @@
-<form action="test.php" method="GET">
-<input type="text" name="link187">
-<input type="hidden" name="prev187" value="gerihome">
-<input type="hidden" name="lang187" value="cy-en">
-<input type="Submit">
-</form>
-<?php
-//echo $_SERVER["REMOTE_ADDR"] . " } { " . $_SERVER["HTTP_USER_AGENT"] ;
-?>
