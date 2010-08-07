3a8de4b0e0c91bd32bb89236e08cb1e42b1c9c38
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-08-07 12:52:32 +0000

    move bn-en to incubator

--- trunk/apertium-bn-en/dev/proper-noun/create-proper-noun-dix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# proper-noun speling
-./create-monodix-proper-noun-enclitic.py > /tmp/proper-noun.bn.dix
-../paradigm-chopper.py /tmp/proper-noun.bn.dix > proper-noun.bn.dix
-
-lt-expand proper-noun.bn.dix > proper-noun.bn.expand
-
