67a7f2ce49cea5242e58dc28fc7db0b353c9776a
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-08-07 12:52:32 +0000

    move bn-en to incubator

--- apertium-bn-en/dev/extract-tool/create-extract.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python apertium2extract.py ../../apertium-bn-en.bn.dix | python reformat.py > apertium-bn-en.bn.dix.extract
--- apertium-bn-en/dev/verb/create-verb-dix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# verb speling
-./create-monodix-verb-enclitic.py > /tmp/verb.bn.dix
-../paradigm-chopper.py /tmp/verb.bn.dix > verb.bn.dix
-
-lt-expand verb.bn.dix > verb.bn.expand
-
