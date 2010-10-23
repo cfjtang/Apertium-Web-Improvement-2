95499cbae0cada4e58d86d5a24b246464d2e7d41
hectoralos <hectoralos@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-10-23 17:10:38 +0000

    Aldono de tekstaro kaj prov-skriptoj

---
+++ apertium-eo-es/nova_versio.sh
@@ -0,0 +1,9 @@
+date=`date +%y%m%d`;
+cp apertium-eo-es.es.dix apertium-eo-es.es.dix.$date
+cp apertium-eo-es.eo.dix apertium-eo-es.eo.dix.$date
+cp apertium-eo-es.eo-es.dix apertium-eo-es.eo-es.dix.$date
+cp apertium-eo-es.es-eo.t1x apertium-eo-es.es-eo.t1x.$date
+cp apertium-eo-es.es-eo.t2x apertium-eo-es.es-eo.t2x.$date
+cp apertium-eo-es.es-eo.t3x apertium-eo-es.es-eo.t3x.$date
+cp nova_traduko.txt aktuala_traduko.txt
+cp aktuala_traduko.txt aktuala_traduko.txt.$date
