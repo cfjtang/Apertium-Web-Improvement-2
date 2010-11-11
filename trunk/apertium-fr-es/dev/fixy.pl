340d82e54ff87849d2cc5950954355ec8bcb0ec0
jimregan <jimregan@72bbbca6-d526-0410-a7d9-f06f51895060>
2010-11-11 15:16:47 +0000

    missed another common problem paradigm

--- trunk/apertium-fr-es/dev/fixy.pl
+++ trunk/apertium-fr-es/dev/fixy.pl
@@ -12,12 +12,12 @@ my ($lm, $out, $in);
 
 while (<>)
 {
-	if (m!<e lm="([^"]*)" a="eleka"><i>([^>]*)</i><par n="dirí/gir__vblex"/></e>!) {
+	if (m!<e lm="([^"]*)" a="eleka"><i>([^>]*)</i><par n="abát/ir__vblex"/></e>!) {
 		$lm=$1;
 		$in=$2;
 		$out = $2; 
 		$out =~ tr/áéíóú/aeiou/; 
-		print "<e lm=\"$lm\" a=\"eleka\"><p><l>$in</l><r>$out</r></p><par n=\"dirí/gir__vblex\"/></e>\n"; 
+		print "<e lm=\"$lm\" a=\"eleka\"><p><l>$in</l><r>$out</r></p><par n=\"abát/ir__vblex\"/></e>\n"; 
 		print STDERR "$1:$2\n";
 	} else {
 		print;
