f01a472d17891174d5dd7da7b37dd71de4c8e29b
vitaka <vitaka@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-01-24 21:40:07 +0000

    More work on itranslate4.eu interface

--- trunk/scaleMT/ScaleMTRouter/src/main/java/org/scalemt/router/ws/EUTranslateResource.java
+++ trunk/scaleMT/ScaleMTRouter/src/main/java/org/scalemt/router/ws/EUTranslateResource.java
@@ -5,14 +5,20 @@
 
 package org.scalemt.router.ws;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -23,21 +29,37 @@ import org.json.JSONObject;
 @Path("itranslate")
 public class EUTranslateResource {
 
+     static Log logger = LogFactory.getLog(EUTranslateResource.class);
+
     @GET
     @Produces("application/json")
+    @Consumes("application/json")
     public String getJSON(@Context HttpServletRequest request) {
         
+        logger.trace("Processing GET request");
+
         String function=request.getParameter("func");
         if("translate".equals(function))
         {
-            JSONObject jo=null;
-            try {
-                jo = new JSONObject(request.getParameter("data"));
-            } catch (JSONException ex) {
-                //TODO: Log, HTTP error code
-                Logger.getLogger(EUTranslateResource.class.getName()).log(Level.SEVERE, null, ex);
+            String data=request.getParameter("data");
+            if(data!=null)
+            {
+                JSONObject jo=null;
+                try {
+                    jo = new JSONObject(data);
+                } catch (JSONException ex) {
+                    //TODO: Log, HTTP error code
+                    logger.warn("Error creating input JSON object", ex);
+                }
+
+                return translate(jo).toString();
             }
-            return translate(jo).toString();
+            else
+            {
+                 //TODO: Log, HTTP error code
+                logger.warn("Data object not provided");
+                return "";
+            }
         }
         else
         {
@@ -48,22 +70,64 @@ public class EUTranslateResource {
     }
 
     @POST
+    //@Consumes({"application/x-www-form-urlencoded", "multipart/form-data"})
     @Produces("application/json")
-    public String postJSON(@Context HttpServletRequest request) {
+    @Consumes("application/json")
+    public String postJSON(InputStream is,@Context HttpServletRequest request) {
+
+        logger.trace("Processing POST request");
 
         String function=request.getParameter("func");
         if("translate".equals(function))
         {
-            //request.getReader();
+            String input=null;
+            BufferedReader reader=null;
+            try
+            {
+                reader=new BufferedReader(new InputStreamReader(is));
+                StringBuilder builder=new StringBuilder();
+                String line;
+                while((line=reader.readLine())!=null)
+                    builder.append(line).append("\n");
 
-            JSONObject jo=null;
-            try {
-                jo = new JSONObject(request.getParameter("data"));
-            } catch (JSONException ex) {
-                //TODO: Log, HTTP error code
-                Logger.getLogger(EUTranslateResource.class.getName()).log(Level.SEVERE, null, ex);
+                input=builder.toString();
+                System.err.println(input);
+
             }
-            return translate(jo).toString();
+            catch(IOException e)
+            {
+               logger.warn("Error reading POST data", e);
+               
+            }
+            finally
+            {
+                if(reader!=null)
+                    try {
+                    reader.close();
+                } catch (IOException ex) {
+                   logger.warn("Error reading POST data", ex);
+                }
+            }
+
+
+            if(input!=null)
+            {
+                logger.trace("POST data: "+input);
+                JSONObject jo=null;
+                try {
+                    jo = new JSONObject(input);
+                } catch (JSONException ex) {
+                    //TODO: Log, HTTP error code
+                    logger.warn("Error creating input JSON object", ex);
+                }
+                return translate(jo).toString();
+            }
+            else
+            {
+                logger.trace("Null POST data");
+            }
+
+             return "";
         }
         else
         {
@@ -75,7 +139,11 @@ public class EUTranslateResource {
 
     private JSONObject translate(JSONObject jo)
     {
-        //TODO: all
+        /*
+       String source=jo.getString("src");
+       String target=jo.getString("target");
+       JSONArray sgms=jo.getJSONArray("sgms");
+       */
         return new JSONObject();
     }
 }
