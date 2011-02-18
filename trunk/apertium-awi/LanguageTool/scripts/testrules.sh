7133036af454b8c186ff2b50c39c1033065047e2
jimregan <jimregan@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-02-18 15:00:28 +0000

    no need to have either languagetool or maligna in SVN

--- trunk/apertium-awi/LanguageTool/scripts/testrules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-java -cp junit.jar:LanguageTool.jar de.danielnaber.languagetool.rules.patterns.PatternRuleTest
-java -cp junit.jar:LanguageTool.jar de.danielnaber.languagetool.ValidateXMLTest
