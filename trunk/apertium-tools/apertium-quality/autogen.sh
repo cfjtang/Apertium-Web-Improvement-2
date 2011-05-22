22e1c077996b49d08bf5799a34d45558b559bf12
bbqsrc <bbqsrc@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-05-22 08:09:29 +0000

    Renamed apertiumqa to apertium-quality, created autogen.sh

---
+++ trunk/apertium-tools/apertium-quality/autogen.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+if [ `id -u` == 0 ] ; then
+	_PREFIXED=0
+else
+	_PREFIXED=1
+fi
+
+_STANDALONE=0
+standalone() {
+	_STANDALONE=1
+	echo "[!] Standalone is a stub for now!"
+	exit 1
+}
+
+prefixed() {
+	_PREFIXED=1	
+}
+
+eval $*
+
+# BEGIN CONFIG
+if [ 'x'$PREFIX == 'x' ] ; then
+	PREFIX="/usr/local"
+fi
+echo "[*] Using prefix: $PREFIX"
+
+if [ 'x'$PYTHON == 'x' ] ; then
+	PYTHON=`type -P python`
+else
+	PYTHON=`type -P $PYTHON`
+fi
+
+if [ 'x'$PYTHON == 'x' ] ; then 
+	echo "[!] Python not found. If not in \$PATH, set PYTHON variable." >&2
+	exit 1
+fi
+echo "[*] Python binary: $PYTHON"
+
+if [ 'x'$VERSION == 'x' ] ; then
+	VERSION=`python -c "import sys; print sys.version[:3]"`
+fi
+
+echo "[*] Python version: $VERSION"
+
+if [ $_PREFIXED -gt 0 ] ; then
+	echo "[*] Doing prefixed (rootless) installation"
+else
+	echo "[*] Doing normal installation (may require root)"
+fi
+echo "Ready to install [y/N]? \c"
+read _ready
+
+if [ ! 'x'$_ready == 'xy' ] ; then
+	echo "Aborting."
+	exit 1
+fi
+
+# BEGIN INSTALLATION
+_install_standalone() {
+	echo "Stub"
+}
+
+_install_prefixed() {
+	export PYTHONPATH="${PREFIX}/lib/python${VERSION}/site-packages"
+	mkdir -p $PYTHONPATH
+	if [ $? -gt 0 ] ; then
+		echo "[!] ${PREFIX} not writable. Aborting."
+		exit 1
+	fi
+	_install
+	echo "[-] It is recommended that you add ${PREFIX}/bin to your PATH variable, and ${PYTHONPATH} to your PYTHONPATH variable."
+}
+
+_install() {
+	echo "[*] Installing..."
+	python setup.py install --prefix=$PREFIX >/dev/null
+}
+
+if [ $_PREFIXED -gt 0 ] ; then
+	_install_prefixed
+else
+	_install
+fi
+
+echo "[*] Installation complete."
