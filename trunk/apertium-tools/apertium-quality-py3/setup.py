3ba7247a66b5f3dfb09832e5a9131469b8c4075d
bbqsrc <bbqsrc@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-06-08 18:02:26 +0000

    Py3

---
+++ trunk/apertium-tools/apertium-quality-py3/setup.py
@@ -0,0 +1,27 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+from os import listdir
+
+install_requires = ['pyyaml', 'argparse', 'matplotlib']
+try:
+	from collections import OrderedDict
+except:
+	install_requires.append('ordereddict')
+
+scripts = [ "bin/" + i for i in listdir("bin") if i[0] != '.' ]
+
+setup(
+	name = "apertium-quality",
+	version = "0.0",
+	packages = find_packages(),
+	scripts = scripts,
+	install_requires = install_requires,
+
+	author = "Brendan Molloy",
+	author_email = "brendan@bbqsrc.net",
+	description = "Apertium Quality Control Framework",
+	license = "CC0",
+	keywords = "apertium nlp quality control framework",
+)
