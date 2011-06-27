16d0854596b4942425349d3c5bcc9c4b6788a9bf
commial <commial@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-06-27 15:39:00 +0000

    Allow the user to change the interface language Put in place the system to managed this changement Put in place the system to integrate these changements with module Change index page to allow it to use translation Write the english, french translation for index page Write the english, french translation for modules FormattedDocumentHandling, SpellGrammarChecking Put in place the system for detect and retains the user language choice

--- trunk/apertium-awi/Post-editingTool/includes/template.php
+++ trunk/apertium-awi/Post-editingTool/includes/template.php
@@ -1,13 +1,13 @@
 <?//coding: utf-8
 /*
-	Apertium Web Post Editing tool
-	Functions for page display
+  Apertium Web Post Editing tool
+  Functions for page display
 	
-	Contributed by Arnaud Vié <unas.zole@gmail.com> for Google Summer of Code 2010
-	Mentors : Luis Villarejo, Mireia Farrús
+  Contributed by Arnaud Vié <unas.zole@gmail.com> for Google Summer of Code 2010
+  Mentors : Luis Villarejo, Mireia Farrús
 
-	Contributed By Mougey Camille <commial@gmail.com> for Google Summer of Code 2011
-	Mentors : Arnaud Vié, Luis Villarejo
+  Contributed By Mougey Camille <commial@gmail.com> for Google Summer of Code 2011
+  Mentors : Arnaud Vié, Luis Villarejo
 */
 
 include_once('strings.php');
@@ -15,32 +15,166 @@ include_once('strings.php');
 function page_header($title, $includes)
 {
 	header ('Content-type: text/html; charset=utf-8');
-?><!DOCTYPE html>
-<html> 
-<head>
-	<meta charset="utf-8">
-	<title><?echo $title;?></title>
-<?	foreach($includes as $file)
-	{
-		if(str_endswith($file, '.js'))
-		{
-?>	<script type="text/javascript" src="<?echo $file . '?v=' . @filemtime($file); ?>"></script>
-<?		}
-		elseif(str_endswith($file, '.css'))
-		{
-?>	<link rel="stylesheet" type="text/css" href="<?echo $file . '?v=' . @filemtime($file);?>" />
-<?		}
-	}
-?>
-</head>
-<body>
-<?
-}
+	?><!DOCTYPE html>
+	<html> 
+		 <head>
+		 <meta charset="utf-8">
+		 <title><?echo $title;?></title>
+						  <?	foreach($includes as $file)
+					  {
+						  if(str_endswith($file, '.js'))
+						  {
+							  ?>	<script type="text/javascript" src="<?echo $file . '?v=' . @filemtime($file); ?>"></script>
+							  <?		}
+						  elseif(str_endswith($file, '.css'))
+						  {
+							  ?>	<link rel="stylesheet" type="text/css" href="<?echo $file . '?v=' . @filemtime($file);?>" />
+							  <?		}
+					  }
+	?>
+	</head>
+		  <body>
+		  <?
+		  }
 
 function page_footer()
 {
-?></body>
-</html>
+	?></body>
+	</html>
+		<?
+		}
+
+/* Interface language */
+
+function avalaible_languages()
+{
+	/* Return an array of languages avalaible, according to 
+	 * templates/avalaible_languages
+	 */
+	$templates_dir = 'templates/';
+	
+	return array_map("trim", file($templates_dir . 'avalaible_languages'));
+}
+
+function get_language()
+{
+	/* Return the current language of the interface
+	 * If no language is set, retrieve him from the browser
+	 * and set $_COOKIE['lang']
+	 * If no language is send by the browser, 'en' is default'
+	 */
+	global $_COOKIE;
+		
+	if (!isset($_COOKIE['lang']) or !in_array($_COOKIE['lang'], avalaible_languages())) {
+		$browser_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+		if (in_array($browser_lang, avalaible_languages())) {
+			setcookie('lang', $browser_lang);
+			$_COOKIE['lang'] = $browser_lang;
+		}
+		else {
+			setcookie('lang','en');
+			$_COOKIE['lang'] = 'en';
+		}
+	}
+		
+	return $_COOKIE['lang'];
+}
+
+function choose_language()
+{
+	/* Write the language menu */
+	$current_language = get_language();
+	?>
+	<form action = "" method = "POST" style='float:right;' onChange='this.submit()'>
+		<select name = 'new_lang'>
+		<?
+		foreach (avalaible_languages() as $lang) {
+		echo "<option label = 'new_lang' value = '".$lang."' ";
+		if ($lang == $current_language)
+			echo "selected = 'selected'";
+		echo ">".$lang."</option>\n";
+	}
+	?>
+	</select>
+		  <noscript><input type="submit" name="submit_language" value="Change" /></noscript>
+		</form>
 <?
 }
+
+function retrieve_info($page, $line)
+{
+	/* Return the line $line of the file templates/$get_language()/$page */
+	$current_language = get_language();
+	$templates_dir = 'templates/';
+
+	$source = array_map("trim", file($templates_dir . $current_language . '/' . $page));
+	return $source[$line];
+}
+
+function get_text($page, $info)
+{
+	/* Return the text of $info of the page $page
+	 * in the language get_language();
+	 */
+	switch ($page) {
+	case 'index':
+		switch ($info) {
+		case 'title':
+			return retrieve_info('index', 0);
+		case 'menu_title':
+			return retrieve_info('index', 1);
+		case 'menu1':
+			return retrieve_info('index', 2);
+		case 'menu2':
+			return retrieve_info('index', 3);
+		case 'supported_format':
+			return retrieve_info('index', 4);
+		case 'translate' :
+			return retrieve_info('index', 5);
+		case 'columns_name' :
+			return retrieve_info('index', 6);
+		default:
+			return '';
+		break;
+		}
+
+	case 'translate':
+		break;
+	default:
+		/* This is a module
+		 * $page = module_modulename
+		 */
+		switch ($info) {
+		case 'name':
+			return retrieve_info($page, 0);
+		case 'description':
+			return retrieve_info($page, 1);
+		case 'button_in':
+			return retrieve_info($page, 2);
+		case 'button_out':
+			return retrieve_info($page, 3);
+		default:
+			return '';
+		break;
+		}
+
+		$module_name = str_replace("module_", '', $page);
+		break;
+	}	
+}
+
+function write_text($page, $info)
+{
+	/* Write the text of $info of the page $page
+	 * in the language get_language();
+	 */
+	echo get_text($page, $info);
+}
+
+/* If the user wants to change the language */
+if (isset($_POST['new_lang']) && in_array($_POST['new_lang'], avalaible_languages())) {
+	setcookie('lang', $_POST['new_lang']);
+	$_COOKIE['lang'] = $_POST['new_lang'];
+}
+
 ?>
