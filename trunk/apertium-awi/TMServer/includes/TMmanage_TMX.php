23dbb8bf714d5c84ede1f270d0ed1309fd574424
commial <commial@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-07-12 13:36:01 +0000

    Add the abstract add_TM function in TMmanage PHP Object Add the add_TM function in TMmanage_TMX PHP Object Add the management of multi-TMX (using TMXMerge) Add a function to generate a TMX file Add a function to manage the current file in database (db/): which file write, which file merge Add a parser, able to detect the source language, the Translation Memory segment, the different target language Fix some security issues Modify the home page to allow the user to add a TMX file

--- trunk/apertium-awi/TMServer/includes/TMmanage_TMX.php
+++ trunk/apertium-awi/TMServer/includes/TMmanage_TMX.php
@@ -13,19 +13,19 @@ include('system.php');
 
 class TMmanage_TMX extends TMmanage
 {
-	private function mergeTMX($files_array)
+	private function mergeTMX($files_array, $src_lang)
 	{
 		/* Merge TMX file in $files_array using tmxmerger_command, 
-		 * and language source: $this->source_language
+		 * and language source: $src_lang
 		 * and return the name of the file generated 
 		 */
 		$tempname = tempnam($this->config['temp_dir'], 'TM_temp_');
-		
-		$command = $this->config['TMXmerge_command'] . ' source=' . $this->source_language;
+		$command = $this->config['TMXmerge_command'] . ' source=' . $src_lang;
 		foreach ($files_array as $file_name)
 			$command .= ' ' . $file_name;
 		
 		$command .= ' ' . $tempname;
+
 		executeCommand($command, '', $return_value, $return_status);
 		
 		return $tempname;
@@ -33,10 +33,11 @@ class TMmanage_TMX extends TMmanage
 	
 	private function write_tmx_file($source, $src_language, $trg_language)
 	{
-		/* Write the tmx file $src_language-$trg_language.tmx with
+		/* Add to the tmx file $src_language-$trg_language.tmx the
 		 * content $source
 		 * Return true if success, false otherwise
 		 */
+		
 		$db_filename = $this->config['database_dir'] . $src_language . '-' . $trg_language . '.tmx';
 
 		if (file_exists($db_filename)) {
@@ -48,12 +49,13 @@ class TMmanage_TMX extends TMmanage
 			
 			if (!$tempname)
 				return FALSE;
-			$new_tmx = $this->mergeTMX(array($tempname, $db_filename));
+			
+			$new_tmx = $this->mergeTMX(array($tempname, $db_filename), $src_language);
 			unlink($tempname);
 			
 			$command = 'mv ' . $new_tmx . ' ' . $db_filename;
 			executeCommand($command, '', $return_value, $return_status);
-			if (!$return_status)
+			if ($return_status)
 				return FALSE;
 
 			return TRUE;
@@ -69,7 +71,76 @@ class TMmanage_TMX extends TMmanage
 		}
 	}
 	
+	private function generateTMX($pretrans_src, $pretrans_dst, $source_language, $target_language)
+	{
+		/* Generate a TMX file for the need of Apertium translation '-m' 
+		 * Return the TMX file source
+		 */
+		$tmx_input = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<tmx version="1.4b">
+	<header srclang="'.$this->source_language.'" segtype="phrase" o-tmf="al" datatype="plaintext" creationtoolversion="1" creationtool="unas" adminlang="en"/>
+	<body>' . "\n";
+		
+		foreach($pretrans_src as $ind => $value)
+		{
+			if(trim($value) != '')
+			{
+				$tmx_input .= '	<tu>
+		<tuv xml:lang="'.$source_language.'">
+			<seg>'. htmlspecialchars($value, ENT_QUOTES, 'UTF-8') .'</seg>
+		</tuv>
+		<tuv xml:lang="'.$target_language.'">
+			<seg>'. htmlspecialchars($pretrans_dst[$ind], ENT_QUOTES, 'UTF-8') .'</seg>
+		</tuv>
+	</tu>' . "\n";
+			}
+		}
+		
+		$tmx_input .= '	</body>
+</tmx>';
+		
+		return $tmx_input;
+	}
 	
+	public function add_TM($source)
+	{
+		/* Add the Translation Memory segment to the database */
+		$xml = simplexml_load_string(str_replace('xml:lang', 'lang', $source));
+		
+		$src_lang = strtolower((string) $xml->header['srclang']);
+		if (!$src_lang)
+			return FALSE;
+		
+		foreach ($xml->body->children() as $tu) {
+			/* For each translation segment, $info[lang] = text 
+			 * in language lang */
+			$info = array();
+			foreach ($tu->children() as $tuv) {
+				$lang = (string) $tuv['lang'];
+				if ($lang == '')
+					continue;
+				elseif (!ctype_alpha($lang))
+					return FALSE;
+				else
+					$info[strtolower($lang)] = (string) $tuv->seg;
+			}
+			/* Add each translation segment in the correct TMX file
+			 * needs src_lang to be in the keys of $info */
+			if (!array_key_exists($src_lang, $info))
+				return FALSE;
+			else {
+				foreach ($info as $lang => $text) {
+					if ($lang == $src_lang)
+						continue;
+					else {
+						if (!$this->write_tmx_file($this->generateTMX(array($info[$src_lang]), array($text), $src_lang, $lang), $src_lang, $lang))
+							return FALSE;
+					}
+				}
+			}
+		}
+		return TRUE;
+	}
 
 	public function get_TM($language_pair = '')
 	{
