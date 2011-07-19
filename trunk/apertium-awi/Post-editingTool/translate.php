76cc1de6c13ca5a23dfdb4d7eb19dcdfaecc8c5b
commial <commial@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-07-19 14:19:27 +0000

    Reorganising translate.php (indent, functions, ..)

--- trunk/apertium-awi/Post-editingTool/translate.php
+++ trunk/apertium-awi/Post-editingTool/translate.php
@@ -19,17 +19,87 @@ init_environment();
 
 $data = escape($_POST);
 
-$to_define = array('text_output', 'text_input', 'input_doc', 'input_doc_type', 'input_doc_name');
-foreach ($to_define as $name_var) {
-	if (!array_key_exists($name_var, $data))
-		$data[$name_var] = '';
+function define_var($array) {
+	/* Foreach element of $array, if $data[element] isn't define, define it */
+	global $data;
+	
+	foreach ($array as $name_var) {
+		if (!array_key_exists($name_var, $data))
+			$data[$name_var] = '';
+	}
 }
-if (!array_key_exists('text_input', $data))
-	$data['text_input'] = '';
+
+function get_action($array) {
+	/* Return the name of the first element of $array corresponding to a $data[element] set */
+	global $data;
+	
+	$action = '';
+	foreach ($array as $name_var) {
+		if (isset($data[$name_var])) {
+			$action = $name_var;
+			break;
+		}
+	}
+			
+	return $action;
+}
+
+function do_action($action_name) {
+	/* Do the action for the corresponding $action_name */
+	global $data;
+	
+	switch ($action_name) {
+	case 'check_input':
+		/* run grammar and spell checking on input
+		 * changes $data['text_input'] */
+		global $source_language;
+		$data['text_input'] = checkForMistakes($data['text_input'], $source_language);
+		break;
+	case 'submit_input':
+		/* translate input
+		 * changes $data['text_output'] */
+		global $trans;
+		$data['text_output'] = $trans->getTranslation($data['text_input']);
+		break;
+	case 'replace_input':
+		$data['text_input'] = str_replace_words($data['pretrans_src'], $data['pretrans_dst'], $data['pretrans_case'], $data['text_input']);
+		break;
+	case 'replace_output':
+		$data['text_output'] = str_replace_words($data['posttrans_src'], $data['posttrans_dst'], $data['posttrans_case'], $data['text_output']);
+		break;
+	case 'check_output':
+		/* run grammar and spell checking on output */
+		global $target_language, $source_language;
+		$data['text_output'] = checkForMistakes($data['text_output'], $target_language, $source_language);
+		break;
+	case 'submit_output_tmx':
+		/* generate translation memory */
+		global $trans;
+		$tmx = $trans->generateTmxOutput(strip_tags($data['text_input']), strip_tags($data['text_output']));
+		send_file('out.tmx', $tmx);
+		break;
+	case 'submit_output':
+		if ($data['input_doc_type'] == 'tif')
+			/* Tif file are managed as TXT file after the translation */
+			$data['input_doc_name'] .= '.txt';
+		$output_file = rebuildFileFromHTML($data['text_output'], $data['input_doc_type'], base64_decode($data['input_doc']));
+		send_file('Translation-' . $data['language_pair'] . '-' . $data['input_doc_name'], $output_file);
+		break;
+	default:
+		break;
+	}
+}
+
+$to_define = array('text_output', 'text_input', 'input_doc', 'input_doc_type', 'input_doc_name');
+define_var($to_define);
 
 /* Define $source_language, $target_language for security issues */
 $target_language = '';
 $source_language = '';
+
+$avalaible_action = array('check_input', 'submit_input', 'replace_input', 'replace_output', 'check_output', 'submit_output_tmx', 'submit_output');
+$action = get_action($avalaible_action);
+do_action($action);
 
 if(isset($_FILES["in_doc"]) AND !($_FILES["in_doc"]["error"] > 0))
 {
@@ -49,6 +119,7 @@ if(isset($data['language_pair']) and is_installed($data['language_pair']))
 	$trans->set_target_language($target_language);
 }
 
+/* TMX Input management */
 if (isset($_FILES["inputTMXFile"]))
 	$trans->set_inputTMX($_FILES["inputTMXFile"]["tmp_name"]);
 
@@ -57,49 +128,7 @@ if (isset($data['inputTMX'])) {
 		$trans->set_inputTMX($data['inputTMX']);
 }
 
-if(isset($data['check_input']))
-{
-	//run grammar and spell checking on input
-	//changes $data['text_input']
-	
-	$data['text_input'] = checkForMistakes($data['text_input'], $source_language);
-}
-elseif(isset($data['submit_input']))
-{
-	//translate input
-	//changes $data['text_output']
-	$data['text_output'] = $trans->getTranslation($data['text_input'] /*, $data['pretrans_src'], $data['pretrans_dst']*/);
-}
-elseif(isset($data['replace_input']))
-{
-	$data['text_input'] = str_replace_words($data['pretrans_src'], $data['pretrans_dst'], $data['pretrans_case'], $data['text_input']);
-}
-elseif(isset($data['replace_output']))
-{
-	$data['text_output'] = str_replace_words($data['posttrans_src'], $data['posttrans_dst'], $data['posttrans_case'], $data['text_output']);
-}
-elseif(isset($data['check_output']))
-{
-	//run grammar and spell checking on output
-	
-	$data['text_output'] = checkForMistakes($data['text_output'], $target_language, $source_language);
-}
-elseif(isset($data['submit_output_tmx']))
-{
-	//generate translation memory
-	$tmx = $trans->generateTmxOutput(strip_tags($data['text_input']), strip_tags($data['text_output']));
-	send_file('out.tmx', $tmx);
-}
-elseif(isset($data['submit_output']))
-{
-	if ($data['input_doc_type'] == 'tif')
-		/* Tif file are managed as TXT file after the translation */
-		$data['input_doc_name'] .= '.txt';
-	$output_file = rebuildFileFromHTML($data['text_output'], $data['input_doc_type'], base64_decode($data['input_doc']));
-	send_file('Translation-' . $data['language_pair'] . '-' . $data['input_doc_name'], $output_file);
-}
-
-
+/* Page display */
 $javascript_header = array(
 	'CSS/textEditor.css',
 	'CSS/style.css',
@@ -125,8 +154,8 @@ display_streamer();
 	</div>
 		
 	<div class="submit_text">
-	<?php
-	foreach (LoadModules() as $module_name)
+<?
+foreach (LoadModules() as $module_name)
 	WriteButtonInput($module_name);
 ?>
 		
@@ -134,66 +163,32 @@ display_streamer();
 	</div>
 		
 	<div class="more_options">
-	<?php
-	if (module_is_load('SearchAndReplace')) {
-		?>
-		<div>
-		<? /*/ Fields for manual translation, which doesn't work at the moment ?>
-		     Manual translations : 
-		     <ul id="pretrans_list">
-		     <?	
-		     if(is_array($data['pretrans_del']))
-		     {
-		     foreach($data['pretrans_del'] as $index => $nothing)
-		     {
-		     unset($data['pretrans_src'][$index]);
-		     unset($data['pretrans_dst'][$index]);
-		     }
-		     }
-	
-		     if(is_array($data['pretrans_src']))
-		     {
-		     foreach($data['pretrans_src'] as $ind => $val)
-		     {
-		     generatePretransLine($val, $data['pretrans_dst'][$ind], $ind);
-		     }
-		     }
-	
-		     if(isset($data['pretrans_add']))
-		     {
-		     generatePretransLine('', '', count($data['pretrans_src']));
-		     }
-		     ?>				</ul>
-		     <input id="pretrans_add" name="pretrans_add" type="submit"  value="+" />
-		     <? //*/ ?>
-		<? write_text('translate', 'manual_replacement'); ?> : 
+<?
+if (module_is_load('SearchAndReplace')) {
+	?>
+	<div>
+<? write_text('translate', 'manual_replacement'); ?> : 
 		<ul id="pretrans_list">
-		<?
-		if(isset($data['pretrans_del']) AND is_array($data['pretrans_del']))
-		{
-			foreach($data['pretrans_del'] as $index => $nothing)
-			{
-				unset($data['pretrans_src'][$index]);
-				unset($data['pretrans_dst'][$index]);
-			}
+<?
+	if(isset($data['pretrans_del']) AND is_array($data['pretrans_del'])) {
+		foreach($data['pretrans_del'] as $index => $nothing) {
+			unset($data['pretrans_src'][$index]);
+			unset($data['pretrans_dst'][$index]);
 		}
+	}
 	
-		if(isset($data['pretrans_src']) AND is_array($data['pretrans_src']))
-		{
-			foreach($data['pretrans_src'] as $ind => $val)
-			{
-				generateReplacementLine('pretrans', $val, $data['pretrans_dst'][$ind], $data['pretrans_case'][$ind], $ind);
-			}
-		}
+	if(isset($data['pretrans_src']) AND is_array($data['pretrans_src'])) {
+		foreach($data['pretrans_src'] as $ind => $val)
+			generateReplacementLine('pretrans', $val, $data['pretrans_dst'][$ind], $data['pretrans_case'][$ind], $ind);
+	}
 	
-		if(isset($data['pretrans_add']))
-		{
-			generateReplacementLine('pretrans', '', '', 'apply', count($data['pretrans_src']));
-		}
-		?>				</ul>
+	if(isset($data['pretrans_add']))
+		generateReplacementLine('pretrans', '', '', 'apply', count($data['pretrans_src']));
+?>
+		</ul>
 		<input id="pretrans_add" name="pretrans_add" type="submit" value="+" />
-		</div>
-		<?php
+	</div>
+<?
 	}
 if (module_is_load('LinkExternalDictionnaries')) {		     
 	echo '<div style="display: none;">' . get_text('translate', 'dictionary') . ' :';
@@ -201,7 +196,7 @@ if (module_is_load('LinkExternalDictionnaries')) {
 	echo '</div>';
 }
 ?>
-<table><tr><td>
+<table>
 <?
 /* Extern Translation Memory Server */
 switch ($config['externTM_type']) {
@@ -217,7 +212,8 @@ case 'TMServer':
 	}
 	
 	?>
-	<p>Extern Translation Memory Server:
+	<tr>
+	<td><p>Extern Translation Memory Server:
 <?
 		 echo '<h4>' . $TM->get_server_url() . '</h4>';
 ?>
@@ -230,29 +226,33 @@ case 'TMServer':
 		echo '>' . $pair . '</option>' . "\n";
 	}
 ?>
-</select></td><td><input type="radio" name="inputTMXtype" value="extern" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'extern')) echo 'checked="1"';?>/></td></tr><tr><td>
+</select></td>
+<td><input type="radio" name="inputTMXtype" value="extern" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'extern')) echo 'checked="1"';?>/></td></tr>
 <?
-	
-	
 	break;
 default:
 	break;
 }
 
 ?>
-<input type="text" name="inputTMX" value="<? echo $trans->get_inputTMX(); ?>" /><input type="submit" name="useTMX" value="Use TMX" /></td><td><input type="radio" name="inputTMXtype" value="URL" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'URL')) echo 'checked="1"';?>/></td></tr>
-<tr><td>
-<input type="file" name="inputTMXFile" /></td><td><input type="radio" name="inputTMXtype" value="FILE" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'FILE')) echo 'checked="1"';?>/></td></tr>
+<tr>
+  <td><input type="text" name="inputTMX" value="<? echo $trans->get_inputTMX(); ?>" /><input type="submit" name="useTMX" value="Use TMX" /></td>
+  <td><input type="radio" name="inputTMXtype" value="URL" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'URL')) echo 'checked="1"';?>/></td>
+</tr>
+<tr>
+  <td><input type="file" name="inputTMXFile" /></td>
+  <td><input type="radio" name="inputTMXtype" value="FILE" <? if (isset($data['inputTMXtype']) && ($data['inputTMXtype'] == 'FILE')) echo 'checked="1"';?>/></td>
+</tr>
 </table>
 </div>
 </td>
 <td>
 <div class="language_select">
 	<select name="language_pair">
-	<?	foreach($language_pairs_list as $pair)
-{
+<?	
+foreach($language_pairs_list as $pair) {
 	?>				<option value="<?echo $pair;?>"<? if(isset($data['language_pair']) AND $data['language_pair'] == $pair) {?> selected="selected"<?} ?>><?echo str_replace('-', ' → ', $pair);?></option>
-	<?	}
+<?	}
 ?>
 	</select>
         <br /><br /><br />
@@ -268,7 +268,7 @@ default:
 	</div>
 		
 	<div class="submit_text">
-<?php
+<?
 	foreach (LoadModules() as $module_name)
 	     WriteButtonOutput($module_name);
 ?>
@@ -278,13 +278,13 @@ default:
 /* Extern Translation Memory Server */
 switch ($config['externTM_type']) {
 case 'TMServer':
-if (isset($data['add_TM'])) {
-        $tmx = $trans->generateTmxOutput(strip_tags($data['text_input']), strip_tags($data['text_output']));
-	if ($TM->send_TM($tmx))
-		echo "<br />TMX file successfully added !<br />";
-	else
-		echo "<br />An error occured...<br />";
-}
+	if (isset($data['add_TM'])) {
+		$tmx = $trans->generateTmxOutput(strip_tags($data['text_input']), strip_tags($data['text_output']));
+		if ($TM->send_TM($tmx))
+			echo "<br />TMX file successfully added !<br />";
+		else
+			echo "<br />An error occured...<br />";
+	}
 ?>
         <input type="submit" name="add_TM" value="Add TMX to the TMServer" />
 <?
@@ -296,40 +296,35 @@ default:
 	</div>
 		
 	<div class="more_options">
-<?php
-	if (module_is_load('SearchAndReplace')) {
+<?
+if (module_is_load('SearchAndReplace')) {
 ?>
 		<div>
-		<? write_text('translate', 'manual_replacement'); ?> : 
+<? 
+	write_text('translate', 'manual_replacement'); ?> : 
 		<ul id="posttrans_list">
 <?
-		if(isset($data['posttrans_del']) AND is_array($data['posttrans_del']))
+	if(isset($data['posttrans_del']) AND is_array($data['posttrans_del'])) {
+		foreach($data['posttrans_del'] as $index => $nothing)
 		{
-			foreach($data['posttrans_del'] as $index => $nothing)
-			{
-				unset($data['posttrans_src'][$index]);
-				unset($data['posttrans_dst'][$index]);
-			}
+			unset($data['posttrans_src'][$index]);
+			unset($data['posttrans_dst'][$index]);
 		}
+	}
 	
-		if(isset($data['posttrans_src']) AND is_array($data['posttrans_src']))
-		{
-			foreach($data['posttrans_src'] as $ind => $val)
-			{
-				generateReplacementLine('posttrans', $val, $data['posttrans_dst'][$ind], $data['posttrans_case'][$ind], $ind);
-			}
-		}
+	if(isset($data['posttrans_src']) AND is_array($data['posttrans_src'])) {
+		foreach($data['posttrans_src'] as $ind => $val)
+			generateReplacementLine('posttrans', $val, $data['posttrans_dst'][$ind], $data['posttrans_case'][$ind], $ind);
 	
 		if(isset($data['posttrans_add']))
-		{
 			generateReplacementLine('posttrans', '', '', 'apply', count($data['posttrans_src']));
-		}
 ?>
 		</ul>
 		<input id="posttrans_add" name="posttrans_add" type="submit" value="+" />
 		</div>
-<?php
+<?
 	}
+}
 if (module_is_load('LinkExternalDictionnaries')) {
 	echo '<div style="display: none;">' . get_text('translate', 'dictionary') . ' :';
 	echo '<select id="dictionary_dst"><option value=""></option></select>';
@@ -348,8 +343,8 @@ if (module_is_load('LinkExternalDictionnaries')) {
 
 <ul style="display:none;" id="list_elements_models">
 <? 
-   generateReplacementLine('pretrans', '', '', 'apply', 'NUM');
-   generateReplacementLine('posttrans', '', '', 'apply', 'NUM');
+generateReplacementLine('pretrans', '', '', 'apply', 'NUM');
+generateReplacementLine('posttrans', '', '', 'apply', 'NUM');
 ?>
 </ul>
 
