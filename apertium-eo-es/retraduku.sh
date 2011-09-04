63575924411f8d24de6189ee2f2ffd4cb0174cb7
hectoralos <hectoralos@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-09-04 22:44:08 +0000

    regles de verbs

--- apertium-eo-es/retraduku.sh
+++
@@ -1,5 +0,0 @@
-date > nova_traduko.err
-make && cat tekstaro/es.crp.txt | time apertium es-eo > nova_traduko.txt 2>> nova_traduko.err
-date >> nova_traduko.err
-diff aktuala_traduko.txt nova_traduko.txt | grep -r '[<>]' > /tmp/crpdiff.txt && for i in `cut -c3-8 /tmp/crpdiff.txt | uniq`; do echo  --- $i ---; grep -r "^ *$i\." tekstaro/es.crp.txt; grep -r " $i\." /tmp/crpdiff.txt; done | less
-
