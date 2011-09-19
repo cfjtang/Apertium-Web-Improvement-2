ac60f6564bce3682e4f0f1d85fca2f70cf2fa181
hectoralos <hectoralos@72bbbca6-d526-0410-a7d9-f06f51895060>
2011-09-19 10:40:41 +0000

    dixs

--- trunk/apertium-eo-es/retraduku_cg.sh
+++ trunk/apertium-eo-es/retraduku_cg.sh
@@ -1,5 +1,6 @@
 date > nova_traduko-cg.err
-make && cat tekstaro/es.crp.txt | time apertium es-eo-cg > nova_traduko.txt 2>> nova_traduko-cg.err
+cat tekstaro/es.crp.txt | time apertium es-eo-cg > nova_traduko.txt 2>> nova_traduko-cg.err
+#cat tekstaro/es.crp.txt | time apertium es-eo-cg-bytecode > nova_traduko.txt 2>> nova_traduko-cg-bc.err
 date >> nova_traduko-cg.err
 diff aktuala_traduko.txt nova_traduko.txt | grep -r '[<>]' > /tmp/crpdiff.txt && for i in `cut -c3-8 /tmp/crpdiff.txt | uniq`; do echo  --- $i ---; grep -r "^ *$i\." tekstaro/es.crp.txt; grep -r " $i\." /tmp/crpdiff.txt; done | less
 
