1c3f59d0b2314b792f42a557e55c4260d0bba9b9
krvoje <krvoje@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-03-09 00:21:33 +0000

    A few missing np's and whatnot

--- trunk/apertium-sh-mk/midterm/obradi.sh
+++ trunk/apertium-sh-mk/midterm/obradi.sh
@@ -4,6 +4,6 @@ cat article.sh.txt | apertium -d .. sh-mk-morph | python ../tagger-to-visl.py | 
 cat article.sh.txt | apertium -d .. sh-mk-chunker > article.sh.chunker.txt
 cat article.sh.txt | apertium -d .. sh-mk-debug > article.sh.debug.txt
 cat article.sh.txt | apertium -d .. sh-mk-postchunk > article.sh.postchunk.txt
-cat article.sh.txt | apertium -d .. sh-mk > article.sh.analysed.txt
-# | python ../tagger-to-visl.py 
+cat article.sh.txt | apertium -d .. sh-mk > article.sh.translated.txt
+# | python ../tagger-to-visl.py
 # grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
