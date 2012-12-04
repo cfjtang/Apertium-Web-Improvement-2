ed86ab63202abaa64135170b3fadaa3f87ba6a5f
ericye16 <ericye16@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-12-04 00:23:47 +0000

    Ported from os to subprocess

--- apertium-dbus/src/command_line.py
+++ apertium-dbus/src/command_line.py
@@ -1,5 +1,5 @@
 def call(cmdline, _in):
-    import os
+    from subprocess import Popen, PIPE
 
     """A convenience function to invoke a subprocess with the
     parameter list name (where the first argument is the name
@@ -8,15 +8,23 @@ def call(cmdline, _in):
     stderr from the subprocess. If stderr is not empty, we
     raise an exception, otherwise we return the contents of
     stdout."""
-    child_in, child_out, child_err = os.popen3(" ".join(cmdline))
+    
+    #Refer to http://docs.python.org/3.3/library/subprocess.html
+    p = Popen(" ".join(cmdline), 
+        shell=True, close_fds=True)
+    
+##    (child_in, child_out, child_err) = (p.stdin, p.stdout, p.stderr)
+    #child_in, child_out, child_err = os.popen3(" ".join(cmdline))
 
-    child_in.write(_in)
-    child_in.close() # You MUST close the child's stdin to get output from some programs
-
-    out = child_out.read()
-    child_out.close()
-    err = child_err.read()
-    child_err.close()
+    p.communicate(input=_in)
+    (out, err) = p.communicate()
+##    child_in.write(_in)
+##    child_in.close() # You MUST close the child's stdin to get output from some programs
+##
+##    out = child_out.read()
+##    child_out.close()
+##    err = child_err.read()
+##    child_err.close()
 
     return out, err
 
