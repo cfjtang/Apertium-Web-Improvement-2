d2d02352de5815dfc2e0356add7457afdc7952b9
ericye16 <ericye16@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-12-07 19:05:31 +0000

    Added back i18n, got threading to work.

--- apertium-tolk/i18n.py
+++ apertium-tolk/i18n.py
@@ -1,14 +1,20 @@
 import gettext
+##from gi.repository import Gtk
 #import Gtk.glade
+import locale
 import prefix
 
 APP='apertium-tolk'
 DIR=prefix.localedir
- 
+
+locale.setlocale(locale.LC_ALL, '')
+locale.bindtextdomain(APP, DIR)
+locale.textdomain(APP)
 gettext.bindtextdomain(APP, DIR)
 gettext.textdomain(APP)
-#Gtk.glade.bindtextdomain(APP, DIR)
-#Gtk.glade.textdomain(APP)
+
+##Gtk.bindtextdomain(APP, DIR)
+##Gtk.textdomain(APP)
 
 _ = gettext.gettext # the i18n function :)
 
