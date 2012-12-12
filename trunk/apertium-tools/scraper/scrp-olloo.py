e3824677e2b4beea5913ae064752574a630636e3
qasim196 <qasim196@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-12-12 01:01:36 +0000

    Made more efficient (closes connections instead of leaving them open, slim)

--- trunk/apertium-tools/scraper/scrp-olloo.py
+++ trunk/apertium-tools/scraper/scrp-olloo.py
@@ -35,23 +35,29 @@ def get_between_all(strSource, strStart, strEnd): #get all the strings between t
     return list
 
 def get_contents(url, encoding):
-    url_md5 = hashlib.md5(url.encode()).hexdigest()
-    if(os.path.isfile('./.cache/' + url_md5)):
-        f = open('./.cache/' + url_md5, 'rb')
-        contents = f.read().decode('utf-8')
-        f.close()
-        return contents
-    else:
-        conn = http.client.HTTPConnection(domain, 80, timeout=60)
-        conn.request("GET", url)
-        res = conn.getresponse()
-        if res.status != 200:
-            print(res.status, res.reason)
-        contents = res.read().decode(encoding)
-        f = open('./.cache/' + url_md5, 'wb')
-        f.write(contents.encode('utf-8'))
-        f.close()
-        return contents
+	while True:
+		try:
+			url_md5 = hashlib.md5(url.encode()).hexdigest()
+			if(os.path.isfile('./.cache/' + url_md5)):
+				f = open('./.cache/' + url_md5, 'rb')
+				contents = f.read().decode('utf-8')
+				f.close()
+				return contents
+			else:
+				conn = http.client.HTTPConnection(domain, 80, timeout=60)
+				conn.request("GET", url)
+				res = conn.getresponse()
+				if res.status != 200:
+				    print(res.status, res.reason)
+				contents = res.read().decode(encoding)
+				f = open('./.cache/' + url_md5, 'wb')
+				f.write(contents.encode('utf-8'))
+				f.close()
+				conn.close()
+				return contents
+		except:
+			continue
+		break
 
 def get_list_urls(): #get all the urls of the pages with links to articles
     contents = get_contents("/modules.php?name=Stories_Archive", 'cp1251')
@@ -77,6 +83,6 @@ def get_urls(): #get all the formatted article URLs
             yield article_url + article_info[0], h.unescape(article_info[1])
 
 '''
-for url, title in get_urls():
+for (url, title) in get_urls():
     print([url, title])
 '''
