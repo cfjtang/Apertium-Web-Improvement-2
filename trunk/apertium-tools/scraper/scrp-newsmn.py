bbc3b1d0df6c074927f2a3e9966722bdfb0d4b1d
jonorthwash <jonorthwash@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-12-24 19:28:58 +0000

    news.mn base script

---
+++ trunk/apertium-tools/scraper/scrp-newsmn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from datetime import date, timedelta
+
+startDate = date(2011, 12, 1)
+endDate = date(2011, 12, 31)
+
+
+def main():
+	global startDate, endDate
+	for (url, title) in getUrls(startDate, endDate):
+		print("%s: %s" % (url, title))
+
+main()
