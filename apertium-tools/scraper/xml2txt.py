152741250a7f803f5252710230d6dd4078818d96
dtvrij74 <dtvrij74@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-12-27 01:12:20 +0000

    added/fixed: -i is now a positional argument no -o given=stdout

--- apertium-tools/scraper/xml2txt.py
+++ apertium-tools/scraper/xml2txt.py
@@ -9,13 +9,17 @@ import codecs
 
 #argparser
 parser = argparse.ArgumentParser(description='xml to txt script')
-parser.add_argument('-i','--corpus_dir', help='corpus directory (input)', required=True)
-parser.add_argument('-o','--output_file', help='name of output_file', required=True)
+parser.add_argument('corpus_dir', metavar='i', help='corpus directory (input)')
+parser.add_argument('-o','--output_file', help='name of output_file', required=False)
 parser.add_argument('-s', '--sentence', action='store_true')
+
+
 
 args = vars(parser.parse_args())
 
-output = open(args['output_file'], 'w')
+if args['output_file'] is not None:
+	output = open(args['output_file'], 'w')
+
 os.chdir(args['corpus_dir'])
 files = os.listdir('.')
 
@@ -30,9 +34,15 @@ for fn in files:
 				itemtxt=str(item.text)
 				tosplit=itemtxt.replace('   ',' ')
 				sentences = re.split('(?<=[.?!])\s+', tosplit)
-				output.write(item.attrib['title']+'\n'+('%s' % '\n'.join(map(str, sentences)))+'\n\n')
+				if args['output_file'] is not None:
+					output.write(item.attrib['title']+'\n'+('%s' % '\n'.join(map(str, sentences)))+'\n\n')
+				else:
+					sys.stdout.write(item.attrib['title']+'\n'+('%s' % '\n'.join(map(str, sentences)))+'\n\n')
 			else: #split by paragraph (default)
-				output.write((item.attrib['title']+'\n'+item.text+'\n\n'))
-
-output.close()
+				if args['output_file'] is not None:
+					output.write((item.attrib['title']+'\n'+item.text+'\n\n'))
+				else:
+					sys.stdout.write((item.attrib['title']+'\n'+item.text+'\n\n'))
+if args['output_file'] is not None:
+	output.close()
 print("Done.")
