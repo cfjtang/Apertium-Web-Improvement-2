b1c09d74ebbdfc32887aa7ec0131ce924ad0a15f
dtvrij74 <dtvrij74@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-01-12 22:07:32 +0000

    fixes kkitap kaz bible's samuel thing

---
+++ apertium-tools/scrapers-misc/kkitapNameFixer.py
@@ -0,0 +1,21 @@
+import sys
+import fileinput
+for line in fileinput.input(inplace=True):
+   # for line in lines:
+    #print(lines)
+    i=0
+    j=0
+    line=line.strip()
+    if "Патшалықтар 1" in line or "Патшалықтар 2" in line:
+        #line="1 Самуил 1"
+        if i<26:
+            line=line.replace("Патшалықтар 1","Самуил 1").strip()
+            line=line.replace("Патшалықтар 2","Самуил 2").strip()
+        i+=1
+    elif "Патшалықтар 3" in line or "Патшалықтар 4" in line:
+
+        line=line.replace("Патшалықтар 3","Патшалықтар 1")
+        line=line.replace("Патшалықтар 4","Патшалықтар 2")
+    print(line)
+
+    #print("".join(lines))
