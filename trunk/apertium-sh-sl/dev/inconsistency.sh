1566ef1de2d268d8c28f860a732fa76222ae99d3
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-04-30 11:42:00 +0000

    update testvoc scripts

--- trunk/apertium-sh-sl/dev/inconsistency.sh
+++ trunk/apertium-sh-sl/dev/inconsistency.sh
@@ -1,7 +1,7 @@
 TMPDIR=/tmp
 VAR=sl-sh_HR
 
-if [[ $1 = "sl-sh" ]]; then
+if [[ $1 = "sl-sh_HR" ]]; then
 
 lt-expand ../apertium-sh-sl.sl.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
@@ -9,8 +9,20 @@ lt-expand ../apertium-sh-sl.sl.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | gr
         apertium-transfer -b ../apertium-sh-sl.sl-sh.t1x  ../sl-sh.t1x.bin |
         apertium-interchunk ../apertium-sh-sl.sl-sh.t2x  ../sl-sh.t2x.bin |
         apertium-postchunk ../apertium-sh-sl.sl-sh.t3x  ../sl-sh.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../$VAR.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        lt-proc -d ../sl-sh_HR.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+
+elif [[ $1 = "sl-sh_SR" ]]; then
+
+lt-expand ../apertium-sh-sl.sl.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+        apertium-pretransfer|
+	lt-proc -b ../sl-sh.autobil.bin | 
+        apertium-transfer -b ../apertium-sh-sl.sl-sh.t1x  ../sl-sh.t1x.bin |
+        apertium-interchunk ../apertium-sh-sl.sl-sh.t2x  ../sl-sh.t2x.bin |
+        apertium-postchunk ../apertium-sh-sl.sl-sh.t3x  ../sl-sh.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../sl-sh_SR.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+
 
 elif [[ $1 = "sh-sl" ]]; then
 
--- trunk/apertium-sh-sl/dev/testvoc.sh
+++ trunk/apertium-sh-sl/dev/testvoc.sh
@@ -1,6 +1,7 @@
 if [[ $1 = "1" ]]; then
 echo "==Slovenian->Serbo-Croatian===========================";
-bash inconsistency.sh sl-sh > /tmp/sl-sh.testvoc; bash inconsistency-summary.sh /tmp/sl-sh.testvoc sl-sh
+bash inconsistency.sh sl-sh_SR > /tmp/sl-sh_SR.testvoc; bash inconsistency-summary.sh /tmp/sl-sh_SR.testvoc sl-sh_SR
+bash inconsistency.sh sl-sh_HR > /tmp/sl-sh_HR.testvoc; bash inconsistency-summary.sh /tmp/sl-sh_HR.testvoc sl-sh_HR
 echo ""
 
 elif [[ $1 = "2" ]]; then
