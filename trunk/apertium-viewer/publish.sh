38fd564ed098169662eb548127f06379a04b7517
nordfalk <nordfalk@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-05-24 00:38:36 +0000

    new

--- trunk/apertium-viewer/publish.sh
+++ trunk/apertium-viewer/publish.sh
@@ -4,7 +4,7 @@ ant
 
 cp dist/apertium-viewer.jar ../../builds/apertium-viewer/
 
-cp dist/lib/lttoolbox.jar ../../builds/apertium-viewer/lib/
+cp dist/lib/*.jar ../../builds/apertium-viewer/lib/
 
 svn commit ../../builds/apertium-viewer/
 
