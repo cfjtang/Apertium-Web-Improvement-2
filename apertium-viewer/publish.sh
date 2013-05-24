6b69cef9f02fcb91c7bf3722e760810616835743
nordfalk <nordfalk@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-05-24 00:29:29 +0000

    new

--- apertium-viewer/publish.sh
+++ apertium-viewer/publish.sh
@@ -1,7 +1,10 @@
-rm -f apertium-viewer.war
-cd dist
-zip -r ../apertium-viewer.war .
-cd ..
-firefox http://javabog.dk:8080/manager/html/undeploy?path=/apertium-viewer  &
-firefox http://javabog.dk:8080/manager/html
+ant clean
+
+ant
+
+cp dist/apertium-viewer.jar ../../builds/apertium-viewer/
+
+cp dist/lib/lttoolbox.jar ../../builds/apertium-viewer/lib/
+
+svn commit ../../builds/apertium-viewer/
 
