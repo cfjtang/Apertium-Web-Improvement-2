3be6f030b020bbc716f910a70c1abeb0cbd6026a
cganas <cganas@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-11-22 01:26:30 +0000

    Added kumukia scraper.

---
+++ trunk/apertium-tools/scrapers-misc/scrp-kumukia.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests, lxml, lxml.html
+
+debug = False
+
+def _url(path):
+  return "http://kumukia.ru/{}".format(path)
+
+base = _url("cat-qumuq.html")
+
+page = lxml.html.fromstring(requests.get(base).text)
+
+cats = []
+
+for link in page.findall(".//a"):
+  for k, v in link.items():
+    if k == "href" and v.startswith("cat-"):
+      cats.append(_url(v))
+
+print("Found {} categories".format(len(cats)))
+
+links = []
+
+for cat in cats:
+  page = lxml.html.fromstring(requests.get(cat).text)
+  for link in page.findall(".//a"):
+    if "title" in link.keys():
+      title = link.get("title")
+      if title == "–>":
+        cats.append(_url(link.get("href")))
+    if link.text_content() == "Далее...":
+      links.append(_url(link.values()[0]))
+
+print("Found {} articles...".format(len(links)))
+
+if debug:
+  for link in links:
+    page = requests.get(link)
+    if page.status_code != 200:
+      print("{} is not reachable...".format(link))
+
+print("\n".join(links))
