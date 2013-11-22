a6ca33ddd9adac3a05ee025bbc9d986fa967ad54
cganas <cganas@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-11-22 02:44:04 +0000

    scrp-kumukia now identifies subcategories.

--- trunk/apertium-tools/scraper/scrp-kumukia.py
+++ trunk/apertium-tools/scraper/scrp-kumukia.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import requests, lxml, lxml.html
 
@@ -7,23 +8,22 @@ debug = False
 def _url(path):
   return "http://kumukia.ru/{}".format(path)
 
-base = _url("cat-qumuq.html")
 
-page = lxml.html.fromstring(requests.get(base).text)
-
-cats = []
-
-for link in page.findall(".//a"):
-  for k, v in link.items():
-    if k == "href" and v.startswith("cat-"):
-      cats.append(_url(v))
-
-print("Found {} categories".format(len(cats)))
-
+cats = [_url("cat-qumuq.html")]
 links = []
 
 for cat in cats:
+
   page = lxml.html.fromstring(requests.get(cat).text)
+
+  sub_page = page.find(".//div[@id='list-subs']")
+  if sub_page is not None:
+    for sub in sub_page.findall(".//a"):
+      for k, v in sub.items():
+        if k == "href" and v.startswith("cat-"):
+          if _url(v) not in cats:
+            cats.append(_url(v))
+
   for link in page.findall(".//a"):
     if "title" in link.keys():
       title = link.get("title")
