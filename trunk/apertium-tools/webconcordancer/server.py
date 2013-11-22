d686887608abba786920eaeba2bdef4bd7740a3f
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-11-22 04:47:31 +0000

    Add table to Web Concodancer

--- trunk/apertium-tools/webconcordancer/server.py
+++ trunk/apertium-tools/webconcordancer/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, codecs, copy
+import sys, codecs, copy, json
 from bottle.bottle import route, run, template, static_file, request, abort
 
 @route('/:file')
@@ -10,13 +10,13 @@ def server_static(file):
 @route('/getResults', method='POST')
 def getResults():
     try:
-        infile = open(request.forms.get('corpus'), 'r')
+        infile = open(request.forms.get('corpus'))
     except:
-       abort(404, 'File not found :' + str(request.forms.get('corpus')))
+        abort(404, 'File not found :' + str(request.forms.get('corpus')))
     findstring = request.forms.get('string')
 
     WINDOW = 50
-    output = ''
+    output = []
 
     for line in infile.read().split('\n'):
         loc = line.find(findstring)
@@ -32,29 +32,29 @@ def getResults():
 
         if start < 0 and end > lgt:
             diff = WINDOW - loc
-            pad = ''
-            for x in range(diff):
-                pad = pad + '&nbsp;'
-            output += '%s&nbsp;%s&nbsp;%s' % ('%', str(pad + line[0:loc]), line[loc:end])
+            #pad = ''
+            #for x in range(diff):
+            #   pad = pad + '&nbsp;'
+            output.append(('%', line[0:loc], line[loc:end]))
             
         elif start > 0 and end > lgt:
-            output += '+ %s&nbsp;%s' % (line[start:loc], line[loc:end])
+            output.append(('+', line[start:loc], line[loc:end]))
             
         elif start < 0 and end < lgt:
             diff = WINDOW - loc
-            pad = '';
-            for x in range(diff):
-                pad = pad + '&nbsp;'
-            output += '-&nbsp;%s&nbsp;%s' % (pad + line[0:loc], line[loc:end])
+            #pad = '';
+            #for x in range(diff):
+            #    pad = pad + '&nbsp;'
+            output.append(('-', line[0:loc], line[loc:end]))
             
         else:
             diff = WINDOW - loc
-            pad = ''
-            for x in range(diff):
-                pad = pad + '&nbsp;'
-            output += '&nbsp;&nbsp;%s&nbsp;%s' % (pad + line[start:loc], line[loc:end])
-        output += '<br>'
+            #pad = ''
+            #for x in range(diff):
+            #    pad = pad + '&nbsp;'
+            output.append(('_', line[start:loc], line[loc:end]))
             
-    return output
-        
+    return json.dumps(output, ensure_ascii=False)
+
+print(sys.version)
 run(host='localhost', port=8080, debug=True)
