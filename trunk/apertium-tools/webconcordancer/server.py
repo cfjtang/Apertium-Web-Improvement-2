f444ffe1c5e9248917f6fcef29d8b0d0d4c2668a
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2013-11-25 20:32:34 +0000

    Add Morphological Analysis Concordancer to Web Interface

--- trunk/apertium-tools/webconcordancer/server.py
+++ trunk/apertium-tools/webconcordancer/server.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
-import sys, codecs, copy, json
+import sys, codecs, copy, json, os
 from bottle.bottle import route, run, template, static_file, request, abort
+from morphAnalysisConcordancer import analyze, getLexicalUnitsStrings, parseLexicalUnitsString, tagSearch, lemmaSearch, surfaceFormSearch, getContext
 
 @route('/:file')
 def server_static(file):
     return static_file(file, root='./')
 
-@route('/getResults', method='POST')
-def getResults():
+@route('/rawCorpusSearch', method='POST')
+def rawCorpusSearch():
     try:
         infile = open(request.forms.get('corpus'), encoding='utf-8') #PYTHON 3
         #infile = open(request.forms.get('corpus')) #PYTHON 2
     except:
-        abort(404, 'File not found :' + str(request.forms.get('corpus')))
+        abort(404, 'File not found : %s' % request.forms.get('corpus'))
     findstring = request.forms.getunicode('string') #getunicode required
 
-    WINDOW = int(request.forms.get('window'))
+    window = int(request.forms.get('window'))
     output = []
 
     for line in infile.readlines():
@@ -25,28 +26,46 @@ def getResults():
             continue
 
         lgt = len(line)
-        mid = lgt / 2
-        text_len = len(findstring)
-
-        start = loc - WINDOW
-        end = loc + WINDOW
+        start = loc - window
+        end = loc + window
 
         if start < 0 and end > lgt:
-            diff = WINDOW - loc
             output.append(('%', line[0:loc], line[loc:end]))
-            
         elif start > 0 and end > lgt:
             output.append(('+', line[start:loc], line[loc:end]))
-            
         elif start < 0 and end < lgt:
-            diff = WINDOW - loc
             output.append(('-', line[0:loc], line[loc:end]))
-            
         else:
-            diff = WINDOW - loc
             output.append(('_', line[start:loc], line[loc:end]))
             
     return json.dumps(output, ensure_ascii=False)
 
+@route('/apertiumSearch', method='POST')
+def apertiumSearch():
+    filename = request.forms.get('corpus')
+    findstring = request.forms.getunicode('string') #getunicode required
+    window = int(request.forms.get('window'))
+    searchType = request.forms.get('mode')
+    output = []
+    
+    if not os.path.isfile(filename):
+        abort(404, 'File not found : %s' % filename)
+    else:
+        analysis = analyze(filename, '/home/apertium/Desktop/apertium-en-es', 'en-es-anmor')
+        lexicalUnitsStrings = getLexicalUnitsStrings(analysis)
+        lexicalUnits = parseLexicalUnitsString(lexicalUnitsStrings)
+        
+        if searchType == 'tag':
+            for (index, lexicalUnit) in tagSearch(lexicalUnits, findstring):
+                output.append((lexicalUnitsStrings[index], getContext(lexicalUnits, index, window=window)))
+        elif searchType == 'lemma':
+            for (index, lexicalUnit) in lemmaSearch(lexicalUnits, findstring):
+                output.append((lexicalUnitsStrings[index], getContext(lexicalUnits, index, window=window)))
+        elif searchType == 'surface':
+            for (index, lexicalUnit) in surfaceFormSearch(lexicalUnits, findstring):
+                output.append((lexicalUnitsStrings[index], getContext(lexicalUnits, index, window=window)))
+        
+        return json.dumps(output, ensure_ascii=False)
+    
 print(sys.version)
 run(host='localhost', port=8080, debug=True)
