9e1d04852b1bacbc077ddce60e3a04aa9b2fa06e
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-01-06 09:27:32 +0000

    Update lexccounter

--- trunk/apertium-tools/lexccounter.py
+++ trunk/apertium-tools/lexccounter.py
@@ -1,69 +1,85 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 
-import argparse, re, sys, urllib.request, logging
+import argparse, re, sys, urllib.request, logging, itertools
+from collections import defaultdict
+
+def cleanLine(line):
+    return re.sub(r'!.*$', '', re.sub(r'%(.)', r'\1', line.strip())).strip()
 
 def countStems(dictionary):
     logger = logging.getLogger(__name__)
-    nums = [0, 0]
     currentLexicon = None
-    validLexicons = []
-    lemmas = set()
+    validLexicons = set()
+    lexicons = defaultdict(lambda: ([], set()))
+
     for lineNo, line in enumerate(dictionary.splitlines()):
-        line = line.strip()
-        if not line.startswith('!') and line:
-            line = line.replace('%', '')
-            if line.startswith('LEXICON'):
-                logger.info('Switching lexicon from %s to %s' % (currentLexicon, line.split()[1]))
-                currentLexicon = line.split()[1]
-            elif currentLexicon == 'Root' and ';' in line:
-                validLexicons.append(line.split(';')[0].strip())
-                logging.info('Counting lexicons %s' % validLexicons)
-            elif any([line.startswith(_) for _ in ['Multichar_Symbols', 'END']]):
-                logging.info('Encountered reserved keyword on L%s: %s' % (lineNo + 1, line))
-            else:
-                if currentLexicon in validLexicons:
-                    try:
-                        if ':' in line:
-                            split = re.findall(r'^(.+?):([^;]+)', line)
-                            if len(split):
-                                split = split[0]
-                                lemma = split[0].strip()
-                                lemmas.add(lemma)
-                                tags = split[1].strip().split()[-1].split('-')
-                                nums[0] += 1
-                                #logger.info('Parsed L%s (%s) as lemma: %s and tags: %s' % (lineNo + 1, line, lemma, tags))
-                        else:
-                            split = line.split(';')[0].strip().split()
-                            lemma = split[0]
-                            lemmas.add(lemma)
-                            tags = split[1].strip().split('-')
-                            nums[0] += 1
+        line = cleanLine(line)
+        if line.startswith('LEXICON'):
+            logger.info('Switching lexicon from %s (%s unique entries, %s pointers) to %s' % (currentLexicon, len(lexicons[currentLexicon][1]), len(lexicons[currentLexicon][0]), line.split()[1]))
+            currentLexicon = line.split()[1]
+        elif not line.startswith('!') and line:
+            try:
+                if len(re.findall(r'\s+', line)) >= 2:
+                    if ':' in line:
+                        split = re.findall(r'^(.+?):([^;]+)', line)
+                        if len(split):
+                            split = split[0]
+                            lemma = split[0].strip()
+                            continuationLexicon = split[1].strip().split()[-1].split('-')
+                            lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
                             #logger.info('Parsed L%s (%s) as lemma: %s and tags: %s' % (lineNo + 1, line, lemma, tags))
-                    except:
+                    elif '-' in line:
+                        split = line.split(';')[0].strip().split()
+                        lemma = split[0]
+                        continuationLexicon = split[1].strip().split('-')
+                        lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
+                        #logger.info('Parsed L%s (%s) as lemma: %s and tags: %s' % (lineNo + 1, line, lemma, tags))
+                elif len(re.findall(r'\s+', line)) == 1:
+                    lexiconPointer = line.split(';')[0].strip()
+                    if ' ' in lexiconPointer:
                         logger.warning('Failed to parse L%s: %s' % (lineNo + 1, line))
-                        nums[1] += 1
-                else:
-                    logger.info('Skipping L%s since in lexicon %s' % (lineNo + 1, currentLexicon))
-    print('Unique lemmas: %s' % len(lemmas))
-    print('Total entries: %s' % nums[0])
-    print('Failed to parse %s entries' % nums[1])
+                    else:
+                        lexicons[currentLexicon][0].append(lexiconPointer)
+                elif currentLexicon:
+                    logger.warning('Failed to parse L%s: %s' % (lineNo + 1, line))
+            except Exception as e:
+                logger.warning('Failed to parse L%s: %s' % (lineNo + 1, line))
+
+    if 'Root' in lexicons:
+        addedLexicons = lexicons['Root'][0]
+        validLexicons.update(addedLexicons)
+        for addedLexicon in addedLexicons:
+            validLexicons.update(lexicons[addedLexicon][0])
+        logging.info('Searching lexicons %s' % validLexicons)
+    else:
+        logger.critical('No Root lexicon found')
+        sys.exit(-1)
+
+    entries = set()
+    for validLexicon in validLexicons:
+        entries.update(lexicons[validLexicon][1])
+
+    print('Unique entries: %s' % len(entries))
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Count stems in a HFST morphological dictionary (lexc)")
     parser.add_argument('uri', help="uri to lexc file")
-    parser.add_argument('-v', '--verbose', help="show progress through dictionary (verbose)", action='store_true', default=False)
+    parser.add_argument('-v', '--verbose', help="show errors dictionary (verbose)", action='store_true', default=False)
+    parser.add_argument('-vv', '--doubleVerbose', help="show progress through dictionary (verbose×2)", action='store_true', default=False)
     args = parser.parse_args()
     
-    if args.verbose:
+    if args.doubleVerbose:
         logging.basicConfig(level=logging.INFO)
-    else:
+    elif args.verbose:
         logging.basicConfig(level=logging.WARNING)
+    else:
+        logging.basicConfig(level=logging.ERROR)
     
     if 'http' in args.uri:
         try:
             dictionary = str((urllib.request.urlopen(args.uri)).read(), 'utf-8')
         except urllib.error.HTTPError:
-            print('Dictionary %s not found' % args.uri)
+            logging.critical('Dictionary %s not found' % args.uri)
             sys.exit(-1)
         countStems(dictionary)
     else:
@@ -71,5 +87,5 @@ if __name__ == '__main__':
             with open(args.uri) as dictionary:
                 countStems(dictionary.read())
         except FileNotFoundError:
-            print('Dictionary %s not found' % args.uri)
+            logging.critical('Dictionary %s not found' % args.uri)
             sys.exit(-1)
