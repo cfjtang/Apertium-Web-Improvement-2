7aad5b4df64dae5f5c53c91b3feb4a3a77c18e6e
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-01-09 12:42:55 +0000

    Add preliminary stem counter bot (for real)

---
+++ trunk/apertium-tools/wiki-tools/bot.py
@@ -0,0 +1,139 @@
+﻿#!/usr/bin/env python3
+
+import argparse, requests, json, logging, sys, re, subprocess, urllib.request
+import xml.etree.ElementTree as etree
+try:
+    from lexccounter import countStems
+except ImportError:
+    logging.error('Unable to import LEXC Stem Counter, proceeding without it.')
+
+baseURL = 'http://wiki.apertium.org/w/api.php'
+svnURL = 'https://svn.code.sf.net/p/apertium/svn/'
+
+def getCounts(uri, dixFormat):
+    try:
+        dixString = str((urllib.request.urlopen(uri)).read(), 'utf-8')
+        dixTree = etree.fromstring(dixString)
+
+        if dixFormat == 'monodix':
+            return {'stems': len(dixTree.findall("section/*[@lm]")), 'paradigms': len(dixTree.find('pardefs').findall("pardef")) }
+        elif dixFormat == 'bidix':
+            return {'stems': len(dixTree.findall("*[@id='main']/e//l"))}
+        elif dixFormat == 'lexc':
+            return {'stems': countStems(dixString)}
+        else:
+            raise ValueError('Invalid format: %s' % dixFormat)
+    except Exception as e:
+        logger.error('Unable to parse counts from %s: %s' % (uri, str(e)))
+        return {}
+
+def getDixNames(uri, dixFormat):
+    try:
+        svnData = str(subprocess.check_output('svn list --xml %s' % uri, stderr=subprocess.STDOUT, shell=True), 'utf-8')
+        return re.findall(r'<name>([^\.]+\.[^\.]+\.(?:meta)?%s)</name>' % dixFormat, svnData, re.DOTALL)
+    except subprocess.CalledProcessError:
+        logging.error('No dix found for %s' % uri)
+        return []
+
+def getRevision(uri):
+    try:
+        svnData = str(subprocess.check_output('svn info -r HEAD %s --xml' % uri, stderr=subprocess.STDOUT, shell=True), 'utf-8')
+        return re.findall(r'revision="([0-9]+)"', svnData, re.DOTALL)[0]
+    except:
+        return None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Apertium Wiki Bot")
+    parser.add_argument('loginName', help="bot login name")
+    parser.add_argument('password', help="bot password")
+    parser.add_argument('pairs', nargs='+', help="Apertium language pairs in the format e.g. incubator/apertium-bg-ru")
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    s = requests.Session()
+
+    try:
+        payload = {'action': 'login', 'format': 'json', 'lgname': args.loginName, 'lgpassword': args.password}
+        authResult = s.post(baseURL, params=payload)
+        authToken = json.loads(authResult.text)['login']['token']
+        logging.debug('Auth token: %s' % authToken)
+
+        payload = {'action': 'login', 'format': 'json', 'lgname': args.loginName, 'lgpassword': args.password, 'lgtoken': authToken}
+        authResult = s.post(baseURL, params=payload)
+        if not json.loads(authResult.text)['login']['result'] == 'Success':
+            logging.critical('Failed to login as %s: %s' % (args.loginName, json.loads(authResult.text)['login']['result']))
+            sys.exit(-1)
+        else:
+            logging.info('Login as %s succeeded' % args.loginName)
+    except Exception as e:
+        logging.critical('Failed to login: %s' % e)
+        sys.exit(-1)
+    try:
+        payload = {'action': 'query', 'format': 'json', 'prop': 'info', 'intoken': 'move', 'titles':'Main Page'}
+        moveTokenResult = s.get(baseURL, params=payload)
+        moveToken = json.loads(moveTokenResult.text)['query']['pages']['1']['movetoken']
+        logging.debug('Move token: %s' % moveToken)
+    except Exception as e:
+        logging.error('Failed to obtain move token: %s' % e)
+
+    try:
+        payload = {'action': 'query', 'format': 'json', 'prop': 'info|revisions', 'intoken': 'edit', 'titles':'Main Page'}
+        editTokenResult = s.get(baseURL, params=payload)
+        editToken = json.loads(editTokenResult.text)['query']['pages']['1']['edittoken']
+        logging.debug('Edit token: %s' % editToken)
+    except Exception as e:
+        logging.error('Failed to obtain edit token: %s' % e)
+
+    SVNRevision = getRevision(svnURL)
+    logging.info('On SVN revision %s' % SVNRevision)
+
+    for pair in args.pairs:
+        langs = pair.split('/')[1].split('-')[1:]
+        pageTitle = 'Apertium-' + '-'.join(langs) + '/stats'
+
+        if len(langs) == 2:
+            dixNames = getDixNames(svnURL + pair, 'dix')
+            dixCounts = {}
+            for dixName in dixNames:
+                dixPair = dixName.split('.')[1].split('-')
+                counts = getCounts('{}{}/{}'.format(svnURL, pair, dixName), 'bidix' if set(langs) == set(dixPair) else 'monodix')
+                for countType, count in counts.items():
+                    dixCounts['-'.join(dixPair + [countType])] = count
+            logging.debug(dixCounts)
+
+            payload = {'action': 'query', 'format': 'json', 'titles': pageTitle, 'prop': 'revisions', 'rvprop': 'content'}
+            viewResult = s.get(baseURL, params=payload)
+            if not 'missing' in list(json.loads(viewResult.text)['query']['pages'].values())[0]:
+                pageContents = list(json.loads(viewResult.text)['query']['pages'].values())[0]['revisions'][0]['*']
+                matchAttempts = re.finditer(r'^\*[^<]+(<section begin=([^/]+)/>.*?$)', pageContents, re.MULTILINE)
+                replacements = {}
+                for matchAttempt in matchAttempts:
+                    countName = matchAttempt.group(2).strip()
+                    if countName in dixCounts:
+                        replacement = "<section begin={0} />{1:,d}<section end={0} /> as of r{2} ~ ~~~~".format(countName, dixCounts[countName], SVNRevision)
+                        replacements[(matchAttempt.group(1))] = replacement
+                for old, new in replacements.items():
+                    pageContents = pageContents.replace(old, new)
+
+                payload = {'action': 'edit', 'format': 'json', 'title': pageTitle, 'text': pageContents, 'bot': 'True', 'contentmodel': 'wikitext', 'token': editToken}
+                editResult = s.post(baseURL, params=payload)
+                if json.loads(editResult.text)['edit']['result'] == 'Success':
+                    logging.info('Update of page %s succeeded' % pageTitle)
+                else:
+                    logging.info('Update of page %s failed: %s' % (pageTitle, json.loads(editResult.text)))
+            else:
+                pageContents = '==Over-all stats=='
+                for dixName, dixCount in dixCounts.items():
+                    pageContents += "\n*'''{0}''': <section begin={0} />{1:,d}<section end={0} /> as of r{2} ~ ~~~~".format(dixName, dixCount, SVNRevision)
+
+                payload = {'action': 'edit', 'format': 'json', 'title': pageTitle, 'text': pageContents, 'bot': 'True', 'contentmodel': 'wikitext', 'token': editToken}
+                createResult = s.post(baseURL, params=payload)
+                if json.loads(editResult.text)['edit']['result'] == 'Success':
+                    logging.info('Update of page %s succeeded' % pageTitle)
+                else:
+                    logging.info('Update of page %s failed: %s' % (pageTitle, json.loads(editResult.text)))
+        elif len(langs) == 1:
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
