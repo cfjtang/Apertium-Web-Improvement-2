ac78253c905420aff0de4427354fa3e39aa9f953
fpetkovski <fpetkovski@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-02-16 13:02:04 +0000

    renaming

--- apertium-hbs-slv/dev/paper/eval/prevedi.sh
+++ apertium-hbs-slv/dev/paper/eval/prevedi.sh
@@ -1,22 +1,22 @@
 # Slovene to BCMS
 echo "jame Slovene to BCMS"
-cat jame.sl.original.txt        |   apertium -d ../../../ sl-sh > sl-bhs/jame.sl-sh
+cat jame.slv.original.txt        |   apertium -d ../../../ slv-hbs > slv-hbs/jame.slv-hbs
 echo "maraton Slovene to BCMS"
-cat maraton.sl.original.txt     |   apertium -d ../../../ sl-sh > sl-bhs/maraton.sl-sh
+cat maraton.slv.original.txt     |   apertium -d ../../../ slv-hbs > slv-hbs/maraton.slv-hbs
 echo "mercator Slovene to BCMS"
-cat mercator.sl.original.txt    |   apertium -d ../../../ sl-sh > sl-bhs/mercator.sl-sh
+cat mercator.slv.original.txt    |   apertium -d ../../../ slv-hbs > slv-hbs/mercator.slv-hbs
 echo "sonce Slovene to BCMS"
-cat sonce.sl.original.txt       |   apertium -d ../../../ sl-sh > sl-bhs/sonce.sl-sh
+cat sonce.slv.original.txt       |   apertium -d ../../../ slv-hbs > slv-hbs/sonce.slv-hbs
 echo "volitve Slovene to BCMS"
-cat volitve.sl.original.txt     |   apertium -d ../../../ sl-sh > sl-bhs/volitve.sl-sh
+cat volitve.slv.original.txt     |   apertium -d ../../../ slv-hbs > slv-hbs/volitve.slv-hbs
 # BCMS to Slovene
 echo "jame BCMS to Slovene"
-cat jame.bhs.original.txt        |   apertium -d ../../../ sh-sl > bhs-sl/jame.sh-sl
+cat jame.hbs.original.txt        |   apertium -d ../../../ sh-hbs > hbs-slv/jame.hbs-slv
 echo "maraton BCMS to Slovene"
-cat maraton.bhs.original.txt     |   apertium -d ../../../ sh-sl > bhs-sl/maraton.sh-sl
+cat maraton.hbs.original.txt     |   apertium -d ../../../ sh-hbs > hbs-slv/maraton.hbs-slv
 echo "mercator BCMS to Slovene"
-cat mercator.bhs.original.txt    |   apertium -d ../../../ sh-sl > bhs-sl/mercator.sh-sl
+cat mercator.hbs.original.txt    |   apertium -d ../../../ sh-hbs > hbs-slv/mercator.hbs-slv
 echo "sunce BCMS to Slovene"
-cat sunce.bhs.original.txt       |   apertium -d ../../../ sh-sl > bhs-sl/sonce.sh-sl
+cat sunce.hbs.original.txt       |   apertium -d ../../../ sh-hbs > hbs-slv/sonce.hbs-slv
 echo "volitve BCMS to Slovene"
-cat volitve.bhs.original.txt     |   apertium -d ../../../ sh-sl > bhs-sl/volitve.sh-sl
+cat volitve.hbs.original.txt     |   apertium -d ../../../ sh-hbs > hbs-slv/volitve.hbs-slv
