837c0005a674c930610d4d46521b8d4bf4a189c9
fpetkovski <fpetkovski@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-02-16 13:03:03 +0000

    more renaming

---
+++ apertium-hbs-slv/dev/paper/eval/slv-hbs/evaluate.sh
@@ -0,0 +1,5 @@
+./apertium-eval-translator.pl -test sonce.slv-hbs -ref sunce.bhs.original.txt > sunce.WER.PER.txt
+./apertium-eval-translator.pl -test mercator.slv-hbs -ref mercator.bhs.original.txt > mercator.WER.PER.txt
+./apertium-eval-translator.pl -test jame.slv-hbs -ref jame.bhs.original.txt > jame.WER.PER.txt
+./apertium-eval-translator.pl -test maraton.slv-hbs -ref maraton.bhs.original.txt > maraton.WER.PER.txt
+./apertium-eval-translator.pl -test volitve.slv-hbs -ref volitve.bhs.original.txt > volitve.WER.PER.txt
