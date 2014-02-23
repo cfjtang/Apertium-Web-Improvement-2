d63f87f7663bbdc39fd37ea100d02972d417e380
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-02-23 21:40:01 +0000

    Allow basing uniqueness on lemma+gloss as well as lemma+continuationLexicon in lexccounter

--- apertium-tools/lexccounter.py
+++ apertium-tools/lexccounter.py
@@ -6,11 +6,14 @@ from collections import defaultdict
 def cleanLine(line):
     return re.sub(r'!.*$', '', re.sub(r'%(.)', r'\1', line.strip())).strip()
 
-def countStems(dictionary):
+def countStems(dictionary, uniqueOn='lemma+continuationLexicon'):
     logger = logging.getLogger("countStems")
     currentLexicon = None
     validLexicons = set()
     lexicons = defaultdict(lambda: ([], set()))
+
+    if uniqueOn not in {'lemma+continuationLexicon', 'lemma+gloss', 'lemma+comment'}:
+        raise ValueError('invalid unique criteria: ' + uniqueOn)
 
     for lineNo, line in enumerate(dictionary.splitlines()):
         line = cleanLine(line)
@@ -21,19 +24,30 @@ def countStems(dictionary):
             try:
                 if len(re.findall(r'\s+', line)) >= 2:
                     if ':' in line:
-                        split = re.findall(r'^(.+?):([^;]+)', line)
+                        split = re.findall(r'^(.+?):([^;]+);(?:\s+!\s+(.+))?', line)
                         if len(split):
                             split = split[0]
                             lemma = split[0].strip()
                             continuationLexicon = split[1].strip().split()[-1].split('-')
-                            lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
-                            logger.debug('Parsed L%s (%s) as lemma: %s and continuations: %s' % (lineNo + 1, line, lemma, continuationLexicon))
+                            gloss = split[2].strip()
+                            if uniqueOn == 'lemma+continuationLexicon':
+                                lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
+                            elif uniqueOn == 'lemma+gloss' or uniqueOn == 'lemma+comment':
+                                lexicons[currentLexicon][1].add((lemma, gloss))
+                            logger.debug('Parsed L%s (%s) as lemma: %s, continuations: %s, and gloss: %s' % (lineNo + 1, line, repr(lemma), repr(continuationLexicon), repr(gloss)))
+                        else:
+                            logger.warning('Failed to parse L%s: %s' % (lineNo + 1, line))
                     else:
                         split = line.split(';')[0].strip().split()
                         lemma = split[0]
                         continuationLexicon = split[1].strip().split('-')
-                        lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
-                        logger.debug('Parsed L%s (%s) as lemma: %s and continuations: %s' % (lineNo + 1, line, lemma, continuationLexicon))
+                        gloss = line.split('!')[1].strip() if '!' in line else None
+                        if uniqueOn == 'lemma+continuationLexicon':
+                            lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))
+                        elif uniqueOn == 'lemma+gloss' or uniqueOn == 'lemma+comment':
+                            lexicons[currentLexicon][1].add((lemma, frozenset(continuationLexicon)))     
+                        
+                        logger.debug('Parsed L%s (%s) as lemma: %s, continuations: %s, and gloss: %s' % (lineNo + 1, line, repr(lemma), repr(continuationLexicon), repr(gloss)))
                 elif len(re.findall(r'\s+', line)) == 1:
                     lexiconPointer = line.split(';')[0].strip()
                     if ' ' in lexiconPointer:
@@ -65,8 +79,9 @@ def countStems(dictionary):
     return len(entries)
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Count stems in a HFST morphological dictionary (lexc)")
+    parser = argparse.ArgumentParser(description="Count unique stems (on lemma and continuation lexicon) in a HFST morphological dictionary (lexc)")
     parser.add_argument('uri', help="uri to lexc file")
+    parser.add_argument('-l', '--uniqLemmaGloss', help="count stems unique on lemma and gloss (comment)", action='store_true', default=False)
     parser.add_argument('-v', '--verbose', help="show errors dictionary (verbose)", action='store_true', default=False)
     parser.add_argument('-vv', '--doubleVerbose', help="show progress through dictionary (verbose×2)", action='store_true', default=False)
     parser.add_argument('-vvv', '--tripleVerbose', help="show detailed progress through dictionary (verbose×3)", action='store_true', default=False)
@@ -87,11 +102,18 @@ if __name__ == '__main__':
         except urllib.error.HTTPError:
             logging.critical('Dictionary %s not found' % args.uri)
             sys.exit(-1)
-        countStems(dictionary)
+
+        if args.uniqLemmaGloss:
+            countStems(dictionary, uniqueOn='lemma+gloss')
+        else:
+            countStems(dictionary)
     else:
         try:
             with open(args.uri) as dictionary:
-                countStems(dictionary.read())
+                if args.uniqLemmaGloss:
+                    countStems(dictionary.read(), uniqueOn='lemma+gloss')
+                else:
+                    countStems(dictionary.read())
         except FileNotFoundError:
             logging.critical('Dictionary %s not found' % args.uri)
             sys.exit(-1)
