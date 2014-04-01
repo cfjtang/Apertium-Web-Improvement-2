d52858b368ea6f635a83cad79dd6460ca1cfee86
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-04-01 02:26:16 +0000

    new testvoc

--- trunk/apertium-hbs-mkd/dev/inconsistency.sh
+++ trunk/apertium-hbs-mkd/dev/inconsistency.sh
@@ -1,23 +1,23 @@
 TMPDIR=/tmp
 
-if [[ $1 = "mk-sh" ]]; then
+if [[ $1 = "mkd-hbs" ]]; then
 
-lt-expand ../apertium-sh-mk.mk.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-hbs-mkd.mkd.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../apertium-sh-mk.mk-sh.t1x  ../mk-sh.t1x.bin  ../mk-sh.autobil.bin |
-        apertium-interchunk ../apertium-sh-mk.mk-sh.t2x  ../mk-sh.t2x.bin |
-        apertium-postchunk ../apertium-sh-mk.mk-sh.t3x  ../mk-sh.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../mk-sh.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-hbs-mkd.mkd-hbs.t1x  ../mkd-hbs.t1x.bin  ../mkd-hbs.autobil.bin |
+        apertium-interchunk ../apertium-hbs-mkd.mkd-hbs.t2x  ../mkd-hbs.t2x.bin |
+        apertium-postchunk ../apertium-hbs-mkd.mkd-hbs.t3x  ../mkd-hbs.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../mkd-hbs.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
-elif [[ $1 = "sh-mk" ]]; then
+elif [[ $1 = "hbs-mkd" ]]; then
 
-lt-expand ../apertium-sh-mk.sh.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-hbs-mkd.hbs.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../apertium-sh-mk.sh-mk.t1x  ../sh-mk.t1x.bin  ../sh-mk.autobil.bin |
-        apertium-interchunk ../apertium-sh-mk.sh-mk.t2x  ../sh-mk.t2x.bin |
-        apertium-postchunk ../apertium-sh-mk.sh-mk.t3x  ../sh-mk.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../sh-mk.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-hbs-mkd.hbs-mkd.t1x  ../hbs-mkd.t1x.bin  ../hbs-mkd.autobil.bin |
+        apertium-interchunk ../apertium-hbs-mkd.hbs-mkd.t2x  ../hbs-mkd.t2x.bin |
+        apertium-postchunk ../apertium-hbs-mkd.hbs-mkd.t3x  ../hbs-mkd.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../hbs-mkd.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 
