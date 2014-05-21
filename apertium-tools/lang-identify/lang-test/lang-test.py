2dd6543714ecd0f08cbf8b86cf981a37f294ccc2
wei2912 <wei2912@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-05-21 13:27:01 +0000

    Update such that tests up to 20000 lines chosen uniformly randomly instead of testing all lines.

--- apertium-tools/lang-identify/lang-test/lang-test.py
+++ apertium-tools/lang-identify/lang-test/lang-test.py
@@ -1,5 +1,8 @@
+NUM_LINES = 20000 # number of lines to test
+
 import cld2full
-import argparse, yaml
+import yaml
+import argparse, random
 
 def get_correct_lang(text, lang):
 	if lang == "zh":
@@ -13,48 +16,69 @@ p.add_argument('-c', '--config', required=True, help='config file')
 
 args = p.parse_args()
 
-counts = {}
-total_counts = {}
-bad_lines = {}
-
 config = yaml.load(open(args.config))
 
 if 'coverage' not in config:
 	config['coverage'] = {}
 
 for lang in config['langs']:
-	if 'coverage' in config['langs'][lang]:
+	if lang in config['coverage']:
 		continue # we already tested this
 
-	counts[lang] = 0
-	total_counts[lang] = 0
-	bad_lines[lang] = []
+	count = 0
+	bad_lines = []
 
-	for filename in config['langs'][lang]:
-			f = open(filename)
-			line = f.readline()
-			while line:
-				if line.strip() == "":
-					line = f.readline()
-					continue
+	f = open(config['langs'][lang])
 
-				if get_correct_lang(line, lang):
-					counts[lang] += 1
-				else:
-					bad_lines[lang].append(line)
-				total_counts[lang] += 1
+	# generate a list of random numbers
+	num_lines = sum(1 for line in f if line.rstrip())
+	if num_lines < NUM_LINES:
+		# iterate through all
+		print("WARNING: Wikipedia corpus text is less than %d. Results may be inaccurate." % NUM_LIMIT)
+		select_lines = range(num_lines)
+	else:
+		# pick NUM_LINES lines from range
+		select_lines = random.sample(xrange(num_lines), NUM_LINES)
+		select_lines.sort()
 
-				print("%d/%d\r" % (counts[lang], total_counts[lang])),
-				
-				line = f.readline()
+	f = open(config['langs'][lang]) # reset pointer to start
 
-	coverage = float(counts[lang])/total_counts[lang]
+	i = 0
+	j = 0
+	for line in f:
+		if line.strip() == "":
+			continue
+
+		# check if it's selected.
+		if i == select_lines[j]:
+			i += 1
+			j += 1 # advance to the next line number
+			if j >= len(select_lines):
+				break # we finished going through all the lines
+		else:
+			i += 1
+			continue
+
+		if get_correct_lang(line, lang):
+			count += 1
+		else:
+			bad_lines.append(line)
+
+		print("%s: %d/%d\r" % (lang, count, len(select_lines))),
+
+	coverage = float(count)/len(select_lines)
 	config['coverage'][lang] = coverage
 
-	print("%s: %d/%d %f" % (lang, counts[lang], total_counts[lang], coverage))
-	for line in bad_lines[lang][:20]:
-		print("\t%s" % line.strip())
+	print("%s: %d/%d %f" % (lang, count, len(select_lines), coverage))
+	if count == 0:
+		print("\tCLD2 does not support %s." % lang)
+	else:
+		lines = random.sample(bad_lines, 20)
+		for line in lines:
+			print("\t%s" % line.strip())
 
 	with open(args.config, 'w') as yaml_file:
 	    dump = yaml.dump(config, allow_unicode=True)
 	    yaml_file.write(dump)
+
+print("All languages have been tested.")
