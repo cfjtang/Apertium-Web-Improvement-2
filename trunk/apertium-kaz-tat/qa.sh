4ec545fe5a4fe381e6dae8f8f572e0260dc3fe01
selimcan <selimcan@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-05-24 09:48:15 +0000

    minor fix in the purpose statement

--- trunk/apertium-kaz-tat/qa.sh
+++ trunk/apertium-kaz-tat/qa.sh
@@ -1,7 +1,6 @@
-# Takes the test name in /tests as an argument, an additional argument
-# if the test requires it (e.g. corpus name) and runs the test.
+# Takes the basename of the test scrpt in /tests as an argument
+# and runs the test.
 
 #!/bin/bash
 
 bash "tests/$1.test"
-
