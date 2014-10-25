a88961aa2060fdd94b70350ec755a970fea9ef6f
jonorthwash <jonorthwash@72bbbca6-d526-0410-a7d9-f06f51895060>
2014-10-25 16:14:20 +0000

    generalised out get_info and print_info

--- apertium-tools/dixcounter.py
+++ apertium-tools/dixcounter.py
@@ -9,31 +9,44 @@ import xml.etree.ElementTree as xml
 import argparse, urllib.request
 
 def print_info(uri, bidix=None):
+    returned = get_info(uri, bidix)
+    if "stems" in returned:
+        print('Stems: %s' % returned['stems'])
+    if "paradigms" in returned:
+        print('Paradigms: %s' % returned['paradigms'])
+	
+def get_info(uri, bidix=None):
     dictX = ""
     if "http" in uri:
         try:
             dictX = str((urllib.request.urlopen(uri)).read(), 'utf-8')
         except:
-            return print(-1)
+            return -1 # FIXME: error handling
 
     else:
         dictX = (open(uri, 'r')).read()
     try:
         tree = xml.fromstring(dictX)
     except:
-        return print(-1)
+        return -1  # FIXME: error handling
 
     if bidix is not None:
         bi = bidix
     else:
         bi = len(tree.findall("pardefs")) == 0 #bilingual dicts don't have pardefs section -- not necessarily true? check /trunk/apertium-en-es/apertium-en-es.en-es.dix
             
+    bi = True
+    out = {}
     if(bi):
-        print('Stems: %s ' % len(tree.findall("*[@id='main']/e//l")))
+        out['stems'] = len(tree.findall("*[@id='main']/e//l"))
+        #print('Stems: %s ' % len(tree.findall("*[@id='main']/e//l")))
     else:
-        print('Stems: %s' % len(tree.findall("section/*[@lm]")))  # there can be sections other than id="main"
+        #print('Stems: %s' % len(tree.findall("section/*[@lm]")))  # there can be sections other than id="main"
+        out['stems'] = len(tree.findall("section/*[@lm]"))  # there can be sections other than id="main"
         if tree.find('pardefs') is not None:
-            print('Paradigms: %s' % len(tree.find('pardefs').findall("pardef")))
+            #print('Paradigms: %s' % len(tree.find('pardefs').findall("pardef")))
+            out['paradigms'] = len(tree.find('pardefs').findall("pardef"))
+    return out
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count unique stems in a monolingual or bilingual dictionary (lttoolbox dix format)")
