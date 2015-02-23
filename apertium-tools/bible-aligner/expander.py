f748b3249e26a526dc58f476617170626d7c3930
jonorthwash <jonorthwash@72bbbca6-d526-0410-a7d9-f06f51895060>
2015-02-23 06:08:15 +0000

    expander and new kaz/kir section names

---
+++ apertium-tools/bible-aligner/expander.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# to use:
+# - run script
+# - paste input
+# - ctrl+d
+# output will be generated and displayed
+# - ctrl+c to quit
+# also:
+# can pipe output from and/or to a file
+
+import fileinput
+import re
+
+numRe = re.compile(".*\".* (\d+)\":.*\".* (\d+)\".*")
+
+for line in fileinput.input():
+	nums = numRe.match(line).groups()
+	if nums[0] == nums[1]:
+		for i in range(1, int(nums[0])+1):
+			#print(str(i)+" ")#, end="")
+			newline = line.strip('\n\r').replace(nums[0], str(i))
+			print(newline)
