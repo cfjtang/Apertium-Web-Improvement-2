af827ede667e8960015495fce433c88873597bfa
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2015-04-13 01:51:33 +0000

    update shyn.ru script

--- trunk/apertium-tools/scrapers-misc/shyn-ru.sh
+++ trunk/apertium-tools/scrapers-misc/shyn-ru.sh
@@ -1,15 +1,16 @@
 
-for i in 201210 201211 201212 201301 201302 201303 201304 201305 201306 201307 201308 201309 201310; do 
-
-	for j in `seq 1 20`; do 
-		echo -e "$i $j" >/dev/stderr; 
-		wget -q -O - "http://www.shyn.ru/?q=archive/$i&page=$j" | grep article\/ | sed 's/<a/@<a/g' | cut -f2 -d'@' | cut -f2 -d'"' | sort -u | grep -v 'comments' >> urls.txt
-	done
-done
+#for i in 2012 2013 2014 2015; do 
+#
+#	for j in `seq 1 120`; do 
+#		echo -e "$i $j" >/dev/stderr; 
+#		wget -q -O - "http://www.shyn.ru/?q=archive/$i&page=$j" | grep article\/ | sed 's/<a/@<a/g' | cut -f2 -d'@' | cut -f2 -d'"' | sort -u | grep -v 'comments' >> urls.txt
+#	done
+#done
 for i in `cat urls.txt | sort -u`; do
 	
-	name=`echo $i | cut -f3 -d'/'`; 
-	echo $name;
-	wget -q -O - "http://www.shyn.ru/$i" > html/$name.html;
+	name=`echo $i | cut -f2 -d'/'`; 
+	md5s=`echo $name | md5sum | cut -f1 -d'-' | sed 's/ *$//g'`
+	echo $md5s" "$name;
+	wget -q -O - "http://www.shyn.ru/$i" > html/$md5s.html;
 done
 
