7ad57671b28998c1e396f13bf829391502f57133
nordfalk <nordfalk@72bbbca6-d526-0410-a7d9-f06f51895060>
2015-05-05 16:02:18 +0000

    Update JPanel forms to be designable in Netbeans 8

--- trunk/apertium-viewer/src/apertiumview/ApertiumViewMain.java
+++ trunk/apertium-viewer/src/apertiumview/ApertiumViewMain.java
@@ -19,17 +19,18 @@ import org.jdesktop.application.SingleFrameApplication;
 /**
  * The main class of the application.
  */
-public class ApertiumViewMain extends SingleFrameApplication {
-    private ApertiumView mainFrame;
+public class ApertiumViewMain {
+	private ApertiumView mainFrame;
 
     
     /**
      * At startup create and show the main frame of the application.
      */
-    @Override protected void startup() {
-        show(mainFrame=new ApertiumView(this));
+    void startup() {
+				mainFrame=new ApertiumView(this);
+        mainFrame.setVisible(true);
 
-        java.awt.Window root= SwingUtilities.getWindowAncestor(mainFrame.mainPanel);
+        java.awt.Window root= mainFrame;
         String geom = mainFrame.prefs.get("geometry",null);
         //System.out.println("geom = " + geom);
         if (geom!= null)  try {
@@ -53,45 +54,33 @@ public class ApertiumViewMain extends SingleFrameApplication {
         // Seems like closing window doesent exit the app - so force exit
         root.addWindowListener(new WindowAdapter() {
         @Override
-          public void windowClosing(WindowEvent e) {shutdown(); System.exit(0);}
+          public void windowClosing(WindowEvent e) {shutdown();}
         });
         
     }
 
-     @Override protected void shutdown() {
+     protected void shutdown() {
          mainFrame.shutdown();
-         java.awt.Window root= SwingUtilities.getWindowAncestor(mainFrame.mainPanel);
+         java.awt.Window root= mainFrame;
         Point location = root.getLocation();
 
         Dimension size = root.getSize();
         mainFrame.prefs.put("geometry", ""+location.x+","+location.y+","+size.width+","+size.height);
         System.err.println("shutdown size = " + size);
+				System.exit(0);
 
-         super.shutdown();
      }
     
-    
-    /**
-     * This method is to initialize the specified window by injecting resources.
-     * Windows shown in our application come fully initialized from the GUI
-     * builder, so this additional configuration is not needed.
-     */
-    @Override protected void configureWindow(java.awt.Window root) {
-    }
-
-    /**
-     * A convenient static getter for the application instance.
-     * @return the instance of ApertiumViewMain
-     */
-    public static ApertiumViewMain getApplication() {
-        return Application.getInstance(ApertiumViewMain.class);
-    }
-
     /**
      * Main method launching the application.
 		 * @param args arguments (ignored)
      */
     public static void main(String[] args) {
-        launch(ApertiumViewMain.class, args);
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					new ApertiumViewMain().startup();
+				}
+			});
     }
 }
