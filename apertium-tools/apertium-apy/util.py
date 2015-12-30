6439aa9c38115d4bb45f6a2c83d56b9a36b2e8ad
sushain97 <sushain97@72bbbca6-d526-0410-a7d9-f06f51895060>
2015-12-30 22:48:24 +0000

    git-svn-sync: Sync repository

--- apertium-tools/apertium-apy/util.py
+++ apertium-tools/apertium-apy/util.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set ts=4 sw=4 sts=4 et :
 
-import sqlite3, re, os, logging, json
+import sqlite3, re, os, logging
 from subprocess import Popen, PIPE
 from datetime import datetime
 import threading
@@ -17,9 +17,6 @@ iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am"
 
 langNamesDBConn = None
 missingFreqsDBConn = None
-
-WIKI_API_URL = 'http://wiki.apertium.org/w/api.php'
-
 
 def toAlpha2Code(code):
     if '_' in code:
@@ -265,69 +262,3 @@ class TranslationInfo:
         self.key = handler.get_argument('key', default = 'null')
         self.ip = handler.request.headers.get('X-Real-IP', handler.request.remote_ip)
         self.referer = handler.request.headers.get('Referer', 'null')
-
-
-# Apertium Wiki utility functions
-def wikiLogin(s, loginName, password):
-    try:
-        payload = {'action': 'login', 'format': 'json', 'lgname': loginName, 'lgpassword': password}
-        authResult = s.post(WIKI_API_URL, params=payload)
-        authToken = json.loads(authResult.text)['login']['token']
-        logging.debug('Auth token: {}'.format(authToken))
-
-        payload = {'action': 'login', 'format': 'json', 'lgname': loginName, 'lgpassword': password, 'lgtoken': authToken}
-        authResult = s.post(WIKI_API_URL, params=payload)
-        if not json.loads(authResult.text)['login']['result'] == 'Success':
-            logging.critical('Failed to login as {}: {}'.format(loginName, json.loads(authResult.text)['login']['result']))
-        else:
-            logging.info('Login as {} succeeded'.format(loginName))
-            return authToken
-    except Exception as e:
-        logging.critical('Failed to login: {}'.format(e))
-
-
-def wikiGetPage(s, pageTitle):
-    payload = {
-        'action': 'query',
-        'format': 'json',
-        'titles': pageTitle,
-        'prop': 'revisions',
-        'rvprop': 'content'
-    }
-    viewResult = s.get(WIKI_API_URL, params=payload)
-    jsonResult = json.loads(viewResult.text)
-
-    if 'missing' not in list(jsonResult['query']['pages'].values())[0]:
-        return list(jsonResult['query']['pages'].values())[0]['revisions'][0]['*']
-
-
-def wikiEditPage(s, pageTitle, pageContents, editToken):
-    payload = {
-        'action': 'edit',
-        'format': 'json',
-        'title': pageTitle,
-        'text': pageContents,
-        'bot': 'True',
-        'contentmodel': 'wikitext',
-        'token': editToken
-    }
-    editResult = s.post(WIKI_API_URL, data=payload)
-    jsonResult = json.loads(editResult.text)
-    return jsonResult
-
-
-def wikiGetToken(s, tokenType, props):
-    try:
-        payload = {
-            'action': 'query',
-            'format': 'json',
-            'prop': props,
-            'intoken': tokenType,
-            'titles': 'Main Page'
-        }
-        tokenResult = s.get(WIKI_API_URL, params=payload)
-        token = json.loads(tokenResult.text)['query']['pages']['1']['%stoken' % tokenType]
-        logging.debug('%s token: %s' % (tokenType, token))
-        return token
-    except Exception as e:
-        logging.error('Failed to obtain %s token: %s' % (tokenType, e))
