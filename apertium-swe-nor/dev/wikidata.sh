3460a6ad84e037242912a074356e6ea7a7c144fd
unhammer <unhammer@72bbbca6-d526-0410-a7d9-f06f51895060>
2016-06-20 08:14:15 +0000

    sort by levdist

--- apertium-swe-nor/dev/wikidata.sh
+++ apertium-swe-nor/dev/wikidata.sh
@@ -10,7 +10,7 @@ test -f globes.jq || bzcat ~/corpora/wikidata-20160229-all.json.bz2 \
 svnn () { jq -c '[.sv,.nn]'|grep -vFe ',null]' -e '[null,' |jq -r '@tsv' ;  }
 
 # Only include those pairs that actually appear in a Wikipedia:
-bzcat ~/corpora/svwiki-20160111-pages-articles.xml.bz2 \
+test -f pairs.tsv || bzcat ~/corpora/svwiki-20160111-pages-articles.xml.bz2 \
     | tr '[:space:][:punct:]' '\n' \
     | gawk -v dict=<(svnn < globes.jq) '
 BEGIN{OFS=FS="\t";
@@ -25,3 +25,38 @@ while(getline<dict)d[$1][$2]++} {
 }
 END{for(src in seen)for(trg in d[src])print seen[src],src,trg}' \
     | sort -nr > pairs.tsv
+
+
+<pairs.tsv gawk '
+BEGIN{OFS=FS="\t"}
+
+# http://awk.freeshell.org/LevenshteinEditDistance
+function levdist(str1, str2,	l1, l2, tog, arr, i, j, a, b, c) {
+	if (str1 == str2) {
+		return 0
+	} else if (str1 == "" || str2 == "") {
+		return length(str1 str2)
+	} else if (substr(str1, 1, 1) == substr(str2, 1, 1)) {
+		a = 2
+		while (substr(str1, a, 1) == substr(str2, a, 1)) a++
+		return levdist(substr(str1, a), substr(str2, a))
+	} else if (substr(str1, l1=length(str1), 1) == substr(str2, l2=length(str2), 1)) {
+		b = 1
+		while (substr(str1, l1-b, 1) == substr(str2, l2-b, 1)) b++
+		return levdist(substr(str1, 1, l1-b), substr(str2, 1, l2-b))
+	}
+	for (i = 0; i <= l2; i++) arr[0, i] = i
+	for (i = 1; i <= l1; i++) {
+		arr[tog = ! tog, 0] = i
+		for (j = 1; j <= l2; j++) {
+			a = arr[! tog, j  ] + 1
+			b = arr[  tog, j-1] + 1
+			c = arr[! tog, j-1] + (substr(str1, i, 1) != substr(str2, j, 1))
+			arr[tog, j] = (((a<=b)&&(a<=c)) ? a : ((b<=a)&&(b<=c)) ? b : c)
+		}
+	}
+	return arr[tog, j-1]
+}
+
+$1 > 1 && $2!~/^[0-9]*$/ && $3!~/^[0-9]*$/ { print levdist($2,$3),$0}
+' | sort -nr > lev.tsv
