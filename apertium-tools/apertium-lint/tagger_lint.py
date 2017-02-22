86f51c0cb7a6199760f01ebc3c0c534b6ae15611
unhammer <unhammer@72bbbca6-d526-0410-a7d9-f06f51895060>
2017-02-22 08:45:43 +0000

    parser 1/2

--- apertium-tools/apertium-lint/tagger_lint.py
+++ apertium-tools/apertium-lint/tagger_lint.py
@@ -35,6 +35,81 @@ def taggerErrors(errorsConf):
 				raise NotImplementedError("Method %s not implemented" % key)
 			method()
 
+def parseLabels():
+
+	tagsetData = {}
+	multTagset = {}
+	taggerPath = "./tagger"
+	tagsetPath = ".//tagset"
+
+	for tagset in tree.findall(tagsetPath):
+		for label in tagset.iterchildren():
+			if label.tag == 'def-label':			#Singular labels
+				labelDict = {}
+				try: labelName = label.attrib['name']
+				except KeyError: labelName = None
+
+				try: labelClosed = label.attrib['closed']
+				except KeyError: labelClosed = False
+
+				tagList = []
+
+				for tagsItem in label.iterchildren():
+
+					try: lemma = tagsItem.attrib['lemma']
+					except KeyError: lemma = None
+
+					try: tags = tagsItem.attrib['tags']
+					except KeyError : tags = None
+
+					tagList.append((lemma, tags))
+
+				labelDict['name'] = labelName
+				labelDict['closed'] = labelClosed
+				labelDict['tagsItem'] = tagList
+
+				tagsetData[labelName] = labelDict
+			
+			if label.tag == 'def-mult':
+				labelDict = {}
+				try: labelName = label.attrib['name']
+				except KeyError: labelName = None
+
+				print(labelName)
+
+				try: labelClosed = label.attrib['closed']
+				except KeyError: labelClosed = False
+
+				tagList1, tagList2 = [], []
+
+				for sequence in label.iterchildren():
+					for tagsItem in sequence.iterchildren():
+
+						if tagsItem.tag == 'tags-item' :
+							try: lemma = tagsItem.attrib['lemma']
+							except KeyError: lemma = None
+							try: tags = tagsItem.attrib['tags']
+							except KeyError : tags = Non
+
+						if tagsItem.tag == 'label-item' :
+							try: label = tagsItem.attrib['label']
+							except KeyError: label = None
+
+						tagList1.append((lemma, tags))
+						tagList2.append(label)
+
+				labelDict['name'] = labelName
+				labelDict['closed'] = labelClosed
+				labelDict['label-item'] = tagList2
+				labelDict['tags-item'] = tagList1
+							
+				tagsetData[labelName] = labelDict
+
+
+	for x in tagsetData:
+		print(x)
+		print(tagsetData[x])
+
 def main(arg1):
 	"""
 	Main function, handles the lint's workflow
@@ -46,5 +121,6 @@ def main(arg1):
 
 	tree = ET.parse(fName)	
 
+	parseLabels()
 if __name__=="__main__":
 		sys.exit(main(sys.argv[1]))
